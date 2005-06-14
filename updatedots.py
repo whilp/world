b4828c514b45388dc28349102e6eab37e0df884b
will <devnull@localhost>
2005-06-14 17:53:02 +0000

    [project @ 2005-06-14 12:53:02 by will] updatedots now just uses symlinks, which allows directories to be automatically updated.

--- updatedots.py
+++ updatedots.py
@@ -15,23 +15,12 @@ def debug(msg=False):
     if debugOn:
 	print "Debug:", msg
 
-def copy(src, dst):
-    os.umask(077)
-    if os.path.exists(dst):
-	newName = ''.join((os.path.split(dst)[1], '.BAK'))
-	debug(newName)
-	newDst = os.path.join(os.path.split(dst)[0], newName)
-	debug(newDst)
-	if os.path.exists(newDst):
-	    if os.path.isdir(newDst):
-		shutil.rmtree(newDst)
-	    else:
-		os.remove(newDst)
-	os.rename(dst, newDst)
-    if os.path.isdir(src):
-	shutil.copytree(src, dst)
-    else:
-	shutil.copy(src, dst)
+def link(src, dst):
+    try:
+	os.symlink(src, dst)
+    except OSError:
+	os.unlink(dst)
+	os.symlink(src, dst)
 	
 for item in dirContents:
     if os.path.isdir(item) or item in execList:
@@ -39,10 +28,11 @@ for item in dirContents:
     else:
 	os.chmod(item, 0600)
     dst = os.path.join(home, ''.join(('.', item)))
+    src = os.path.join(os.getcwd(), item)
     if item in skipList or item.startswith('.'):
 	debug(item)
 	pass
     else:
-	print "Copying", item, "to", dst
-	copy(item, dst)
+	print "Linking", item, "to", dst
+	link(src, dst)
 
