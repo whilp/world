66828075b6aa57d2b689e63a739ae70fdee9d92d
will <devnull@localhost>
2005-07-22 23:22:10 +0000

    [project @ 2005-07-22 18:22:10 by will]

---
+++ scripts/mykeys.sh
@@ -0,0 +1,9 @@
+# I created this so that my page up/down keys would be loaded on boot.
+# wcmaier / 2005.06.16
+
+setkeycodes e06a 127
+setkeycodes e069 126
+
+echo "keycode 127 = Next" | loadkeys
+echo "keycode 126 = Prior" | loadkeys
+echo "keycode 58 = Escape" | loadkeys
