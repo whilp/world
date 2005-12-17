584c988de8e688e2f12e07fa9a27134899f24840
will <devnull@localhost>
2005-12-17 17:16:41 +0000

    [project @ 2005-12-17 11:16:41 by will]

--- scripts/screenmchk.sh
+++ scripts/screenmchk.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
-# Checks important mailboxes; returns an "M" if new mail has
-# arrived. This probably seems useless, and it is, except it gives
-# me a little popup in screen when I have important unread mail.
+# Makes a little 'M' appear in screen's hardstatus when I've got new
+# mail.
 
+# Programs used.
+GREP=/usr/bin/egrep
+WC=/usr/bin/wc
+FIND=/usr/bin/find
+CUT=/usr/bin/cut
+SED=/usr/bin/sed
 
+# Settings.
+MAILDIR=$HOME/Maildir
+BOXES=
+
+# Check that there's a maildir here; not much point running
+# otherwise.
+if [ ! -d "${MAILDIR}" ]; then
+    return 1
+fi
+
+NUMBER="$(${FIND} ${MAILDIR} -path "*new/[^/]*" -type f | \
+    ${WC} -l | \
+    ${SED} 's/ //g')"
+if [ "${NUMBER}" -gt 0 ]; then
+    echo ${NUMBER}
+fi
