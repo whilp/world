bc725e3ef66e699583186b5341ada00323b2799d
will <devnull@localhost>
2006-05-11 19:08:23 +0000

    [project @ 2006-05-11 14:08:23 by will]

--- scripts/tunes.sh
+++ scripts/tunes.sh
@@ -1,18 +1,36 @@
 #!/bin/sh
 
+ENVCACHE=${HOME}/.environment
 MPD_CONF=/etc/mpd.conf
 MPD_STATE=$(grep state ${MPD_CONF} | sed -e 's/.*"\(\/.*\)"$/\1/')
-if [ "$(pgrep -lf 'bin/mpd')" ]; then
-    MPD_HOST="localhost"
-else
-    MPD_HOST="messenger"
+if [ -r "${ENVCACHE}" ]; then
+    # If there's a file with cached environment variables on this
+    # machine, look for relevant stuff and source it.
+    grep '^PREF_MPD_HOST=[^ ]\+$' "${ENVCACHE}" | while read SETTING; do
+        eval "${SETTING}"
+    done
 fi
-#MPD_HOST="messenger"
+
+HOSTS="${MPD_HOST} ${PREF_MPD_HOST} messenger localhost"
+
+for HOST in $HOSTS; do 
+    # Work through the host list in order, testing connections to
+    # the server for each. If we get something (_anything_) back on
+    # stdin, set that host as MPD_HOST and break.
+    if [ -n "$(MPD_HOST="${HOST}" mpc 2>/dev/null)" ]; then
+        MPD_HOST=${HOST}
+        break
+    fi
+done
+
+# If we don't have anything to talk to, die now.
+[ "${MPD_HOST}" ] || exit 1
+
 MPC_COMMAND=
 INCREMENT=7
 CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
 MINVOL=30
-MAXVOL=80
+MAXVOL=75
 FORMAT="[[%artist% - ]%title%|%name%|%file%]"
 
 if [ $# -eq 0 ]; then
@@ -75,27 +93,33 @@ else
             done
             ;;
         slide)
-            INC=1
             # The modulo bits are intended to smooth the slide;
             # there may be a better method, but it seems rather
             # efficient to do it this way.
+            # TODO: This doesn't make sense; if $INC only
+            # increments when [$((INC % 2)) -eq 0], the 
+            INCINC=0
             if [ ${CURVOL} -gt 50 ]; then
+                INC=1
                 while [ ${CURVOL} -gt ${MINVOL} ]; do
                     # Slide down
                     MPD_HOST=${MPD_HOST} mpc volume -${INC} >/dev/null 2>&1
                     CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-                    if [ $((INC % 2)) -eq 0 ]; then
+                    if [ $((INCINC % 7)) -eq 0 -a ${INC} -lt 4 ]; then
                         INC=$((INC + 1))
                     fi
+                    INCINC=$((INCINC + 1))
                 done
             else
+                INC=4
                 while [ ${CURVOL} -lt ${MAXVOL} ]; do
                     # Slide up
                     MPD_HOST=${MPD_HOST} mpc volume +${INC} >/dev/null 2>&1
                     CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-                    if [ $((INC % 2)) -eq 0 ]; then
-                        INC=$((INC + 1))
+                    if [ $((INCINC % 7)) -eq 0 -a ${INC} -gt 1 ]; then
+                        INC=$((INC - 1))
                     fi
+                    INCINC=$((INCINC + 1))
                 done
             fi
             ;;
