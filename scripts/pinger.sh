2e2d8a8ee25c085a008479542d3a20b208229c05
will <devnull@localhost>
2006-05-25 23:02:39 +0000

    [project @ 2006-05-25 17:59:26 by will]

--- scripts/pinger.sh
+++ scripts/pinger.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-TARGET=$1
+# TODO:
+# Arg parsing
+# Input sanitization
+# Check ssh availability?
+# Comments!
+
+# From rader's /usr/local/etc/pinger:
+# usage:  pinger [args] host [... hostN]
+#         pinger [args] -r X-Y basename
+# -nb            no bell
+# -c             count (default is 5 packets)
+# -w             deadline (default is 1 second)
+# -s             packetsize (default is 100 bytes)
+# -t X           % packet loss threshold (default is > 60)
+# -r X-Y         for hosts X through Y
+# --once         ping once and then exit
+# --domain name  append "name" to host names
+
+TARGET=$1       # Should fail if nothing's specified
 DURATION=
 INTERVAL=3
 VERBOSE=1
