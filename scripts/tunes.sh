df76bdaf381981a1b5cfa5dbfde58ddb9af73aa4
will <devnull@localhost>
2006-06-15 22:24:05 +0000

    [project @ 2006-06-15 17:23:56 by will]

--- scripts/tunes.sh
+++ scripts/tunes.sh
@@ -1,6 +1,4 @@
-#!/bin/sh -x
-
-# Works
+#!/bin/sh
 
 ENVCACHE="${HOME}/.environment"
 # MPD_CONF=/etc/mpd.conf
@@ -12,20 +10,13 @@ if [ -r "${ENVCACHE}" ]; then
     PREF_MPD_HOST="$(sed -e '/^PREF_MPD_HOST=/!d; s/PREF_MPD_HOST=//' "${ENVCACHE}")"
 fi
 
-send () {
-    echo $* | nc ${MPD_HOST} ${MPD_PORT}
-}
-curvol () {
-    send status | grep '^volume:' | cut -d ':' -f 2 2>/dev/null
-}
-
 HOSTS="${MPD_HOST} ${PREF_MPD_HOST} localhost messenger"
 
 for HOST in $HOSTS; do 
     # Work through the host list in order, testing connections to
     # the server for each. If we get something (_anything_) back on
     # stdin, set that host as MPD_HOST and break.
-    if [ -n "$(MPD_HOST="${HOST}" send 2>/dev/null)" ]; then
+    if [ -n "$(MPD_HOST="${HOST}" mpc 2>/dev/null)" ]; then
         MPD_HOST=${HOST}
         break
     fi
@@ -34,15 +25,9 @@ done
 # If we don't have anything to talk to, die now.
 [ "${MPD_HOST}" ] || exit 1
 
-
-[ -x "$(which mpc 2>/dev/null)" ] && \
-    HAVE_MPC=1 || \
-    HAVE_MPC=
 MPC_COMMAND=
-MPD_PORT=6600
 INCREMENT=7
-CURVOL="$(curvol)"
-#CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
+CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
 MINVOL=30
 MAXVOL=75
 FORMAT="[[%artist% - ]%title%|%name%|%file%]"
@@ -51,26 +36,18 @@ if [ $# -eq 0 ]; then
     # Open ncmpc in a new window if it's not running; otherwise,
     # pause/resume playback.
     if [ ! "$(pgrep -lf "ncmpc --host ${MPD_HOST}")" ]; then
-        term -e -T MPD "ncmpc --host ${MPD_HOST}"
+        term -tn xterm-color -T MPD -e "ncmpc --host ${MPD_HOST}"
     else
-        send pause
-        #MPD_HOST=${MPD_HOST} mpc toggle
+        MPD_HOST=${MPD_HOST} mpc toggle
     fi
 else
     # Twiddle a knob.
     case $1 in
-        stats|clear|add|next|stop)
+        toggle|stats|clear|add|next|prev|stop)
             MPC_COMMAND="$1"
             ;;
-        prev)
-            MPC_COMMAND="previous"
-            ;;
-        toggle)
-            MPC_COMMAND="pause"
-            ;;
         volume)
-            MPC_COMMAND="setvol $2"
-            #MPC_COMMAND="volume $2"
+            MPC_COMMAND="volume $2"
             ;;
         up|down|query|slide|ncmpc)
             # just ignore -- they're special
@@ -80,15 +57,14 @@ else
             exit 1
             ;;
     esac
-    [ "${MPC_COMMAND}" ] && send ${MPC_COMMAND} >/dev/null 2>&1 && exit
-    #[ "${MPC_COMMAND}" ] && MPD_HOST=${MPD_HOST} mpc ${MPC_COMMAND} 2>/dev/null 2>&1 && exit
+    [ "${MPC_COMMAND}" ] && MPD_HOST=${MPD_HOST} mpc ${MPC_COMMAND} 2>/dev/null 2>&1 && exit
 
     case $1 in
         query)
-            [ "${HAVE_MPC}" ] && MPD_HOST=${MPD_HOST} mpc --format "${FORMAT}"
+            MPD_HOST=${MPD_HOST} mpc --format "${FORMAT}"
             ;;
         ncmpc)
-            term -T MPD -e "ncmpc --host ${MPD_HOST}"
+            term -tn xterm-color -T MPD -e "ncmpc --host ${MPD_HOST}"
             ;;
     esac
 
@@ -96,20 +72,14 @@ else
     case $1 in
         down)
             while [ ${CURVOL} -gt ${MINVOL} ]; do
-                NEWVOL=$((CURVOL - INCREMENT))
-                send setvol ${NEWVOL} >/dev/null 2>&1
-                #MPD_HOST=${MPD_HOST} mpc volume -${INCREMENT} >/dev/null 2>&1
-                CURVOL="$(curvol)"
-                #CURVOL=$(send status | sed -e 's/[^0-9]//g')
+                MPD_HOST=${MPD_HOST} mpc volume -${INCREMENT} >/dev/null 2>&1
+                CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
             done
             ;;
         up)
             while [ ${CURVOL} -lt ${MAXVOL} ]; do
-                NEWVOL=$((CURVOL + INCREMENT))
-                send setvol ${NEWVOL} >/dev/null 2>&1
-                #MPD_HOST=${MPD_HOST} mpc volume +${INCREMENT} >/dev/null 2>&1
-                CURVOL="$(curvol)"
-                #CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
+                MPD_HOST=${MPD_HOST} mpc volume +${INCREMENT} >/dev/null 2>&1
+                CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
             done
             ;;
         slide)
@@ -123,11 +93,8 @@ else
                 INC=1
                 while [ ${CURVOL} -gt ${MINVOL} ]; do
                     # Slide down
-                    NEWVOL=$((CURVOL - INC))
-                    send setvol ${NEWVOL} >/dev/null 2>&1
-                    #MPD_HOST=${MPD_HOST} mpc volume -${INC} >/dev/null 2>&1
-                    CURVOL=$(curvol)
-                    #CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
+                    MPD_HOST=${MPD_HOST} mpc volume -${INC} >/dev/null 2>&1
+                    CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
                     if [ $((INCINC % 7)) -eq 0 -a ${INC} -lt 4 ]; then
                         INC=$((INC + 1))
                     fi
@@ -137,11 +104,8 @@ else
                 INC=4
                 while [ ${CURVOL} -lt ${MAXVOL} ]; do
                     # Slide up
-                    NEWVOL=$((CURVOL + INC))
-                    send volume ${CURVOL} >/dev/null 2>&1
-                    #MPD_HOST=${MPD_HOST} mpc volume +${INC} >/dev/null 2>&1
-                    CURVOL=$(curvol)
-                    #CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
+                    MPD_HOST=${MPD_HOST} mpc volume +${INC} >/dev/null 2>&1
+                    CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
                     if [ $((INCINC % 7)) -eq 0 -a ${INC} -gt 1 ]; then
                         INC=$((INC - 1))
                     fi
