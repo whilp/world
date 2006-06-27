2634ba9275dd88c51eb0f7565d5ef5da644ec5c7
will <devnull@localhost>
2006-06-27 03:00:00 +0000

    [project @ 2006-06-26 22:00:00 by will] Pause the server if requested before locking.

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/sh -x
 
 ENVIRONMENT=${HOME}/.environment
 LOCK_VOL=''
+if [ $1 -eq "pause" ]; then
+    PAUSE=1
+else
+    PAUSE=
+fi
+
 
 if [ -x "$(which tunes)" -a "$(grep '^PREF_LOCK_VOL=1' ${ENVIRONMENT})" ]; then
     LOCK_VOL=1
 fi
 
-[ "${LOCK_VOL}" ] && tunes slide &
-xlock -mode blank
-[ "${LOCK_VOL}" ] && tunes slide &
+if [ "${PAUSE}" ]; then
+    [ "${LOCK_VOL}" ] && tunes slide && tunes toggle &
+    xlock -mode blank
+    [ "${LOCK_VOL}" ] && tunes toggle
+    [ "${LOCK_VOL}" ] && tunes slide &
+else
+    [ "${LOCK_VOL}" ] && tunes slide &
+    xlock -mode blank
+    [ "${LOCK_VOL}" ] && tunes slide &
+fi
