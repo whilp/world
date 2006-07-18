6b2cfff3aeb0f255c848f31be10c13147bc6c731
will <devnull@localhost>
2006-07-18 22:50:39 +0000

    [project @ 2006-07-18 17:50:39 by will] - EXCLUDE_RE: Exclude directories during backup.  - Silence mkdirs.  - Don't link old kernels.  - Don't automatically run pkg_add -iu.  - Prompt before running the merge (so we can prune directories from    the merge source).  - Mergemaster: ignore /etc/motd

--- scripts/obsd-update.sh
+++ scripts/obsd-update.sh
@@ -12,12 +12,13 @@ PKG_PATH=${PKG_PATH:-${MY_PP}}
 VERSION=39
 WORKDIR="${HOME}/BSD"
 BACKUPDIR=/home/backup
+EXCLUDE_RE="/var/space"
 
 case "x$1" in
    xpre)
 
 	# Fetch installsets.
-	mkdir "${WORKDIR}"
+	mkdir "${WORKDIR}" 2>/dev/null
 	cd "${WORKDIR}"
 	ftp ${PKG_PATH%%packages*}/$(machine)/\*
 
@@ -25,7 +26,7 @@ case "x$1" in
 	sudo cp /bsd /bsd.old
 	sudo cp /bsd.rd /bsd.rd.old
 	sudo cp bsd.rd /bsd.rd
-	sudo ln -f /bsd.rd /bsd        # In case the thing doesn't want to boot from bsd.rd.
+	echo sudo ln -f /bsd.rd /bsd        # In case the thing doesn't want to boot from bsd.rd.
 
 	# Reboot into the upgrade process. Follow the instructions (like an
 	# install).
@@ -34,26 +35,37 @@ case "x$1" in
 	;;
    xpost)
 	# Boot into the new system and update packages.
-	sudo pkg_add -iu
+	echo sudo pkg_add -iu
 
 	# Backup and merge changes to /etc, /var and /root (which aren't
 	# covered by the upgrade process). I think /dev is covered by the
 	# install process.
 	cd "${WORKDIR}"
-	mkdir merge
+	mkdir merge 2>/dev/null
 	tar xvzf "etc${VERSION}.tgz" -C merge/
 
-	sudo mkdir "${BACKUPDIR}"
+        set -x
+	sudo mkdir "${BACKUPDIR}" 2>/dev/null
 	DIRS="etc var root"
 	for DIR in ${DIRS}; do
-	    sudo tar cvzf "${BACKUPDIR}/${DIR}-$(date "+%Y%m%d-%H%M%S").tgz" "/${DIR}"
+	    sudo sh -c "tar cvzf \"${BACKUPDIR}/${DIR}-$(date "+%Y%m%d-%H%M%S").tgz\" \
+                $(find /${DIR} -maxdepth 1 -mindepth 1 | grep -vE "${EXCLUDE_RE}")"
+	    #sudo tar cvzf "${BACKUPDIR}/${DIR}-$(date "+%Y%m%d-%H%M%S").tgz" "/${DIR}"
 	done
+        set +x
+
+        # Prepare to merge.
+        echo "===> We're about to merge changes to /etc, /var/, and /root."
+        echo "===> Remove files from ${WORKDIR}/merge now to avoid useless"
+        echo "===> merging (eg var/www/htdocs/...)."
+        echo "===> Press [Enter] to continue."
+        read RESPONSE
 
 	# Merge changes.
 	sudo pkg_add mergemaster
-	sudo mergemaster -rt "${WORKDIR}/merge"
+	sudo IGNORE_MOTD=yes mergemaster -rt "${WORKDIR}/merge"
 
 	# Rebuild the password databases (/etc/pwd.db and /etc/spwd.db).
-	sudo pwd_mkdb /etc/master.passwd
+	# sudo pwd_mkdb /etc/master.passwd
 	;;
 esac
