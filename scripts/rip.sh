3e3463300bb1025f6b3cc87d15193709e36f5c1f
Will Maier <willmaier@ml1.net>
2006-08-17 12:23:36 -0500

    Add a reference encoding implementation (in comments); clean up comments; add notes/TODO about children.

--- scripts/rip.sh
+++ scripts/rip.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 MUSIC=$HOME/Music
+DIR=
 
 LINENR=0
 echo 'cddb' | cdio 2>/dev/null | while read LINE; do
     LINENR=$((LINENR + 1))
     if [ ${LINENR} -eq 1 ]; then
-        # Ellen Allien / Berlinette(newage)
         export ARTIST=${LINE%% / *}
         export ALBUM=${LINE##* / }
         export ALBUM=${ALBUM%%\(*\)}
         export DIR="${MUSIC}/${ARTIST}/${ALBUM}"
         echo "===> Beginning rip in ${DIR}."
         mkdir -p "${DIR}"
+        cd "${DIR}"
     elif [ ${LINENR} -gt 3 ]; then
-        #     1   3:54.24  Alles Sehen
         TRACKNR=$(echo ${LINE} | cut -d ' ' -f 1)
         if [ ${TRACKNR} -lt 10 ]; then
             TRACKNR="0${TRACKNR}"
@@ -24,11 +24,36 @@ echo 'cddb' | cdio 2>/dev/null | while read LINE; do
 
         FILENAME="${DIR}/${TRACKNR} - ${TITLE}.wav"
         echo "===> Ripping track ${TRACKNR}: ${TITLE}."
+        # TODO: It would be nice to:
+        # a) encode only if we know cdparanoia successfully ripped a
+        # track.
+        # b) monitor the children (ie wait until they've died to
+        # actually quit.
+        # This is complicated by the fact that this entire while
+        # loop runs in a subshell (due to the pipe), and so wait(1)
+        # doesn't see the subshell's children. Boo.
         cdparanoia -q ${TRACKNR} "${FILENAME}"
+        nice -n 10 flac --silent --best -T ARTIST="${ARTIST}" \
+            -T NUMBER="${TRACKNR}" \
+            -T ALBUM="${ALBUM}" \
+            -T TITLE="${TITLE}" "${FILENAME}" &
     fi
     LINENR=$((LINENR + 1))
 done
+wait
+exit
 
-echo "===> Beginning FLAC encode for ${ARTIST} - ${ALBUM}."
-cd ${DIR}
-flac --silent --best *
+# cd "${DIR}"
+# ARTIST=${PWD%/*}; ARTIST=${ARTIST##*/}
+# ALBUM=${PWD##*/}
+# echo "===> Beginning FLAC encode for ${ARTIST} - ${ALBUM}."
+# flac --silent --best *
+# for FILE in *.flac; do
+#     NUMBER=${FILE%% - *}
+#     TITLE=${FILE##* - }; TITLE=${TITLE%.flac}
+# 
+#     metaflac --set-tag=ARTIST="${ARTIST}" \
+#             --set-tag=NUMBER="${NUMBER}" \
+#             --set-tag=ALBUM="${ALBUM}" \
+#             --set-tag=TITLE="${TITLE}" "${FILE}"
+# done
