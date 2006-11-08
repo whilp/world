5152cd05d6813321eb84debb1106a62c303c99e6
Will Maier <willmaier@ml1.net>
2006-11-08 08:26:19 -0600

    Use system volume.

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -14,15 +14,15 @@ if [ -x "$(which tunes)" -a "$(grep '^PREF_LOCK_VOL=1' ${ENVIRONMENT})" ]; then
 fi
 
 if [ "${PAUSE}" ]; then
-    [ "${LOCK_VOL}" ] && vol -t && vol -t &
+    [ "${LOCK_VOL}" ] && vol -m 
     #[ "${LOCK_VOL}" ] && tunes slide && tunes toggle &
     xlock -mode blank
     [ "${LOCK_VOL}" ] && vol -t
     #[ "${LOCK_VOL}" ] && tunes toggle
-    [ "${LOCK_VOL}" ] && vol -t &
+    #[ "${LOCK_VOL}" ] && vol -t &
     #[ "${LOCK_VOL}" ] && tunes slide &
 else
-    [ "${LOCK_VOL}" ] && vol -t &
+    [ "${LOCK_VOL}" ] && vol -m &
     #[ "${LOCK_VOL}" ] && tunes slide &
     xlock -mode blank
     [ "${LOCK_VOL}" ] && vol -t &
