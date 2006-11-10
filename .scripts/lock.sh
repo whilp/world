8329ba6b66f92c9a67e38ed41c5a9202c40b1def
Will Maier <willmaier@ml1.net>
2006-11-10 16:18:12 -0600

    Always run vol in the background.

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -14,10 +14,10 @@ if [ -x "$(which tunes)" -a "$(grep '^PREF_LOCK_VOL=1' ${ENVIRONMENT})" ]; then
 fi
 
 if [ "${PAUSE}" ]; then
-    [ "${LOCK_VOL}" ] && vol -m 
+    [ "${LOCK_VOL}" ] && vol -m &
     #[ "${LOCK_VOL}" ] && tunes slide && tunes toggle &
     xlock -mode blank
-    [ "${LOCK_VOL}" ] && vol -t
+    [ "${LOCK_VOL}" ] && vol -t &
     #[ "${LOCK_VOL}" ] && tunes toggle
     #[ "${LOCK_VOL}" ] && vol -t &
     #[ "${LOCK_VOL}" ] && tunes slide &
