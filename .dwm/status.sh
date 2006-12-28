59649b22be21bb80d25f4c1828d752087b4e0427
Will Maier <willmaier@ml1.net>
2006-12-27 18:59:06 -0600

    Make arithmetic work with linux sh.

--- .dwm/status.sh
+++ .dwm/status.sh
@@ -5,7 +5,7 @@ checkdate () {
     echo $(date "+%a %d %b %H:%M %Z %Y")
 }
 checkmpd () {
-    I=$(echo 'currentsong' | nc ${MPD_HOST} ${MPD_PORT})
+    I=$(echo 'currentsong' | nc -w 1 ${MPD_HOST} ${MPD_PORT})
     O=$(echo ${I} | sed -e 's/^.* Artist: \(.*\) Album: \(.*\) Title: \(.*\) Pos:.*$/\1 (\2) - \3/')     
 
     if [ "$(echo ${O} | grep '^OK.*OK$')" ]; then
@@ -32,6 +32,7 @@ D=0
 
 # MPD counters.
 MPD_MAX=30
+MPD_BAD=0
 L=0
 R=${MPD_MAX}
 M=0
@@ -46,7 +47,7 @@ while :; do
         DATE_OUT="$(checkdate)"
         D=0
     else
-        D="$((D + 1))"
+        D="$(($D + 1))"
     fi
     OUT="[${DATE_OUT}]"
 
@@ -59,20 +60,20 @@ while :; do
     fi
     if [ -z "${MPD_IN}" ]; then
         # We've gotten two bad checks from MPD in a row.
-        MPD_BAD=$((MPD_BAD + 1))
+        MPD_BAD=$(($MPD_BAD + 1))
     fi
     # Handle scrolling if necessary.
     if [ -n "${MPD_IN}" ]; then
         MPD_LEN="$(len "${MPD_IN}")"
         if [ -z "${MPD_PAUSE}" -a "$((MPD_LEN - 1))" -ge "${MPD_MAX}" ]; then
             # Scroll.
-            L=$((L + 1))
-            R=$((R + 1))
+            L=$(($L + 1))
+            R=$(($R + 1))
             MPD_OUT="$(echo ${MPD_IN} | cut -c "${L}-${R}")"
             if [ "${L}" -le "1" ]; then
                 # We're at the beginning of the string.
                 MPD_PAUSE="${MPD_OUT}"
-            elif [ "$((R + 1))" -ge "${MPD_LEN}" ]; then
+            elif [ "$(($R + 1))" -ge "${MPD_LEN}" ]; then
                 # We're at the end of the string.
                 L=0
                 R=${MPD_MAX}
