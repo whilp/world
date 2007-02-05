efbdf95029ef96ccb432cafdae8c4fbddfac5128
Will Maier <willmaier@ml1.net>
2007-02-05 09:45:28 -0600

    Add a scrolling generator.

--- .dwm/status.py
+++ .dwm/status.py
@@ -18,6 +18,39 @@ if os.path.exists(os.path.expanduser(lockfile)):
 fifo = "~/.dwm/fifo"
 f = open(os.path.expanduser(fifo), 'w')
 
+def scroll(fetch, max, out=False):
+    """ Generator to scroll text input.
+    """
+    # Create counters.
+    l = -1
+    m = max - 1
+    out = ''
+    count = 0
+
+    # Generator loop.
+    while True:
+        if not out:
+            # Fetch input.
+            out = fetch()
+
+        if len(out) <= max:
+            yield out, count
+            count += 1
+            if count > 30:
+                count = 0
+                print fetch()
+        else:
+            if m >= len(out):
+                # Reset.
+                l = -1
+                m = max
+                out = fetch()
+            else:
+                # Increment values.
+                l += 1
+                m += 1
+                yield out[l:m], l, m
+
 def nowPlaying(server='localhost', port=6600):
     """ Query a running MPD server.
     """
@@ -25,13 +58,17 @@ def nowPlaying(server='localhost', port=6600):
     mpd = socket()
     mpd.connect((server, port))
 
+    # Find out what's playing.
     mpd.send('currentsong\n')
     resp = mpd.recv(1000)
     if 'OK MPD' not in resp:
+        # Can't talk to the server.
         return None
 
+    # Create a dictionary based on the output from MPD.
     info = dict([(x.split(': ', 1)) for x in resp.split('\n') if len(x.split(': ', 1)) == 2])
 
+    # Fill in interesting values.
     artist = info.get('Artist')
     album = info.get('Album')
     title = info.get('Title')
