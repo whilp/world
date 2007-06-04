20efa659c23087bd6a2d4bbaf171f273209998a1
Will Maier <willmaier@ml1.net>
2007-06-04 12:13:22 -0500

    Redo for mplayer.

--- .scripts/tunes.sh
+++ .scripts/tunes.sh
@@ -1,122 +1,44 @@
 #!/bin/sh
 
-ENVCACHE="${HOME}/.environment"
-# MPD_CONF=/etc/mpd.conf
-# MPD_STATE=$(grep state ${MPD_CONF} | sed -e 's/.*"\(\/.*\)"$/\1/')
-MPD_PORT=6600
-if [ -r "${ENVCACHE}" ]; then
-    # If there's a file with cached environment variables on this
-    # machine, look for relevant stuff and source it.
-    PREF_MPD_HOST="$(sed -e '/^PREF_MPD_HOST=/!d; s/PREF_MPD_HOST=//' "${ENVCACHE}")"
-fi
+DIR="/home/will/music"
 
-HOSTS="${MPD_HOST} ${PREF_MPD_HOST} localhost messenger"
-
-for HOST in $HOSTS; do 
-    # Work through the host list in order, testing connections to
-    # the server for each. If we get something (_anything_) back on
-    # stdin, set that host as MPD_HOST and break.
-    true
-    [ "$?" -eq 0 ] && MPD_HOST=${HOST} mpc >/dev/null 2>&1
-    [ "$?" -eq 0 ] && MPD_HOST=${HOST}
-done
-
-# If we don't have anything to talk to, die now.
-if [ ! "${MPD_HOST}" ]; then
-    echo "@@Nothing@@" 
-    exit 1
-fi
-
-MPC_COMMAND=
-INCREMENT=7
-CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-MINVOL=30
-MAXVOL=75
-FORMAT="[[%artist% - ]%title%|%name%|%file%]"
-
-if [ $# -eq 0 ]; then
-    # Open ncmpc in a new window if it's not running; otherwise,
-    # pause/resume playback.
-    if [ ! "$(pgrep -lf "ncmpc --host ${MPD_HOST}")" ]; then
-        term -tn vt100 -T MPD -e "ncmpc --host ${MPD_HOST}"
+menu () {
+    if [ -n "$1" ]; then
+        dmenu -b -p $1
     else
-        MPD_HOST=${MPD_HOST} mpc toggle
+        dmenu -b
     fi
-else
-    # Twiddle a knob.
-    case $1 in
-        toggle|stats|clear|add|next|prev|stop)
-            MPC_COMMAND="$1"
-            ;;
-        volume)
-            MPC_COMMAND="volume $2"
-            ;;
-        seek)
-            MPC_COMMAND="seek $2"
-            ;;
-        up|down|query|slide|ncmpc)
-            # just ignore -- they're special
-            ;;
-        *)
-            echo "Bad command: $1."
-            exit 1
-            ;;
-    esac
-    [ "${MPC_COMMAND}" ] && MPD_HOST=${MPD_HOST} mpc ${MPC_COMMAND} 2>/dev/null 2>&1 && exit
+}
 
-    case $1 in
-        query)
-            MPD_HOST=${MPD_HOST} mpc --format "${FORMAT}"
-            ;;
-        ncmpc)
-            term -tn vt100 -T MPD -e "ncmpc --host ${MPD_HOST}"
-            ;;
-    esac
+lsmusic () {
+    for ITEM in "$1"/*; do
+        [ -d "${ITEM}" ] && echo "${ITEM##*/}/"
+        [ -f "${ITEM}" ] && echo "${ITEM##*/}"
+    done
+}
 
-    # Special tricky slides
-    case $1 in
-        down)
-            while [ ${CURVOL} -gt ${MINVOL} ]; do
-                MPD_HOST=${MPD_HOST} mpc volume -${INCREMENT} >/dev/null 2>&1
-                CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-            done
-            ;;
-        up)
-            while [ ${CURVOL} -lt ${MAXVOL} ]; do
-                MPD_HOST=${MPD_HOST} mpc volume +${INCREMENT} >/dev/null 2>&1
-                CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-            done
-            ;;
-        slide)
-            # The modulo bits are intended to smooth the slide;
-            # there may be a better method, but it seems rather
-            # efficient to do it this way.
-            # TODO: This doesn't make sense; if $INC only
-            # increments when [$((INC % 2)) -eq 0], the 
-            INCINC=0
-            if [ ${CURVOL} -gt 50 ]; then
-                INC=1
-                while [ ${CURVOL} -gt ${MINVOL} ]; do
-                    # Slide down
-                    MPD_HOST=${MPD_HOST} mpc volume -${INC} >/dev/null 2>&1
-                    CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-                    if [ $((INCINC % 7)) -eq 0 -a ${INC} -lt 4 ]; then
-                        INC=$((INC + 1))
-                    fi
-                    INCINC=$((INCINC + 1))
-                done
-            else
-                INC=4
-                while [ ${CURVOL} -lt ${MAXVOL} ]; do
-                    # Slide up
-                    MPD_HOST=${MPD_HOST} mpc volume +${INC} >/dev/null 2>&1
-                    CURVOL=$(MPD_HOST=${MPD_HOST} mpc volume | sed -e 's/[^0-9]//g')
-                    if [ $((INCINC % 7)) -eq 0 -a ${INC} -gt 1 ]; then
-                        INC=$((INC - 1))
-                    fi
-                    INCINC=$((INCINC + 1))
-                done
-            fi
-            ;;
-    esac
-fi
+choosefile () {
+    CHOICE=$(lsmusic "${DIR}" | menu "${DIR}:")
+    if [ -z "${CHOICE}" ]; then
+        exit
+    elif [ "${CHOICE}" = ".." ]; then
+        DIR=$(dirname ${DIR})
+        choosefile "${DIR}"
+    elif [ -d "${DIR}/${CHOICE}" ]; then
+        DIR="${DIR}/${CHOICE%%/*}"
+        choosefile "${DIR}"
+    else
+        FILE="${DIR}/${CHOICE}"
+        case "${FILE}" in
+            *.radio)
+                cat ${FILE}
+                ;;
+            *)
+                echo ${FILE}
+                ;;
+        esac
+    fi
+}
+
+FILE=$(choosefile)
+echo loadfile $FILE >> ~/.mplayer/fifo
