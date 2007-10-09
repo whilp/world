30064b7dfe6251a6c27dbe49779fbdce394ad7cd
Will Maier <willmaier@ml1.net>
2007-10-09 07:29:08 -0500

    Zap debugging prints.

--- .scripts/password.py
+++ .scripts/password.py
@@ -210,4 +210,4 @@ if __name__ == '__main__':
     # Generate the passwords.
     for i in range(NUM_PASSWORDS):
         password = ''.join(pgen.generate())
-        print password, mq_filter(password)
+        print password
