77b27465552c76a1caa5aeb328e8595f33a9be8a
Will Maier <willmaier@ml1.net>
2008-12-03 14:01:45 -0600

    Add a script to pull basketball schedules.

---
+++ .scripts/bbsched2rem.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from urllib2 import urlopen
+
+from icalendar import Calendar
+from pytz import timezone
+
+SCHED_URL = "http://uwbadgers.com/ical_schedules/116/UW-Athletics.ics"
+TIMEZONE = timezone('America/Chicago')
+
+sched = urlopen(SCHED_URL).read()
+
+cal = Calendar.from_string(sched)
+events = [x for x in cal.walk() if x.name == 'VEVENT']
+
+for event in events:
+    # Convert to localtimezone.
+    start = event['DTSTART'].dt
+    if hasattr(start, 'astimezone'):
+        start = start.astimezone(TIMEZONE)
+
+    start = start.strftime('%d %b %Y AT %H:%M')
+
+    duration = event['DURATION']
+    summary = event['SUMMARY'].replace('Basketball-M: ', '')
+    description = ' '.join(event['DESCRIPTION'].split()).replace('Media: ', '')
+
+    print 'REM %s MSG %s %s' % (start, summary, description)
