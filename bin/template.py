2d9dfd776501c419c31c8e52278d2618da1a3d1c
Will Maier <willmaier@ml1.net>
2010-11-16 12:55:46 -0600

    Update template.py.

--- bin/template.py
+++ bin/template.py
@@ -1,58 +1,17 @@
 import logging
 import optparse
-import os
 import sys
-import time
-
-from optparse import Option
 
 try:
     from logging import NullHandler
 except ImportError:
     class NullHandler(logging.Handler):
-        
         def emit(self, record):
             pass
 
-logname = __name__
-if __name__ == "__main__":
-	logname = os.path.splitext(os.path.basename(sys.argv[0]))
-logging.getLogger(logname).addHandler(NullHandler())
+logging.getLogger("template").addHandler(NullHandler())
 
-def getlogger(name, level, stream,
-        fmt="%(asctime)s %(name)s %(message)s",
-        datefmt="%Y-%m-%dT%H:%M:%S%z"):
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    handler = logging.StreamHandler(stream)
-    formatter = Formatter(fmt, datefmt)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    return logger
-
-def getcmd(name, commands):
-    """Resolve *name* to a command registered in *commands*.
-
-    *name* should be a string; *commands* is a dictionary mapping full command
-    names to information about each command (help text, usage, etc). If *name*
-    isn't found in *commands* but one (and only one) entry in the dictionary
-    starts with *name*, use that entry as the command. If more or less than one
-    entry matches, raise ValueError.
-
-    Returns a tuple (name, command), where *name* and *command* are the matching
-    key and value from *commands*.
-    """
-    command = commands.get(name, None)
-    if command is not None:
-        return (name, command)
-
-    matches = [c for c in commands if c.startswith(name)]
-    (name,) = matches
-    return (name, commands[name])
-
-def parseargs(args, commands={}):
+def parseargs(args):
     parser = optparse.OptionParser()
     parser.allow_interspersed_args = False
 
@@ -67,65 +26,24 @@ def parseargs(args, commands={}):
         default=0, action="count",
         help="increase the logging verbosity")
 
-    if commands:
-        usage = ["%prog COMMAND\n\nCommands:"]
-        for name, cmd  in commands.items():
-            usage.append("  %14s  %s" % (name, cmd.help))
-        parser.usage = '\n'.join(usage)
-
     (opts, args) = parser.parse_args()
-    if commands and not args:
-        parser.print_help()
-        sys.exit(0)
-    elif not commands:
-        return (None, opts, None, args)
-
-    cmdname = args[0]
-    try:
-        cmdname, command = getcmd(args.pop(0), commands)
-    except ValueError, e:
-        parser.error("invalid command %r" % cmdname)
-
-    subparser = optparse.OptionParser()
-    for opt in command.opts:
-        subparser.add_option(opt)
-
-    subparser.usage = "%%prog %s %s" % (cmdname, cmd.usage)
-
-    (cmdopts, args) = subparser.parse_args(args)
-
-    return (command, opts, cmdopts, args)
-
-class Command(object):
-    help = ""
-    usage = ""
-    opts = []
-
-    def dispatch(self, globalopts, opts, args):
-        """Run the command.
-
-        *globalopts* and *opts* are :class:`optparse.Values` instances; *args*
-        is a list of arguments. Returns None or an integer indicating the status
-        of the command.
-        """
-        pass
-
-commands = {
-}
+    return (opts, args)
 
 def main():
-    (command, opts, cmdopts, args) = parseargs(sys.argv[1:], commands)
+    (opts, args) = parseargs(sys.argv[1:])
     level = logging.WARNING - ((opts.verbose - opts.quiet) * 10)
     if opts.silent:
         level = logging.CRITICAL + 1
 
-    logger = getlogger(logname, level, sys.stderr)
+    format = "%(message)s"
+    logger = logging.getLogger("template")
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(logging.Formatter(format))
+    logger.addHandler(handler)
+    logger.setLevel(level)
 
     try:
-        returned = command.dispatch(opts, cmdopts, args)
-    except TypeError:
-        sys.stderr.write("%s: invalid arguments\n" % logname)
-        returned = 2
+        returned = do_stuff()
     except (KeyboardInterrupt,), e:
         returned = None
 
