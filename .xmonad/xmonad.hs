57c796dd43e9f2d53dd7888a5ad0db7b6668cedb
Will Maier <willmaier@ml1.net>
2010-11-28 18:00:28 -0600

    Use winkey as mod.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -32,7 +32,7 @@ myBorderWidth   = 1
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
-myModMask       = mod1Mask
+myModMask       = mod4Mask
 
 -- The mask for the numlock key. Numlock status is "masked" from the
 -- current modifier status, so the keybindings will work with numlock on or
