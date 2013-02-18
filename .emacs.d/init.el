5eca7f16fc0b9e0871fdc6d62e48c93b7df5ec35
Will Maier <wcmaier@m.aier.us>
2013-02-18 00:19:55 -0600

    Comment and reorganize.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,3 @@
-(global-set-key [remap eval-expression] 'pp-eval-expression)
-(global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
-
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
@@ -8,6 +5,10 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+
+;; pretty-print expression evals.
+(global-set-key [remap eval-expression] 'pp-eval-expression)
+(global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
 
 (autoload 'magit-status' "magit" nil t)
 (global-set-key (kbd "C-x C-g") 'magit-status)
