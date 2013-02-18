8c0cfcb511b1649cf86401a2eb4e22a99617f29d
Will Maier <wcmaier@m.aier.us>
2013-02-17 18:57:51 -0600

    Zap zenburn.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,3 @@
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'zenburn t)
-
 (global-set-key [remap eval-expression] 'pp-eval-expression)
 (global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
 
