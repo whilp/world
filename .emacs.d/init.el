0604c289e3a50ff81543db1d68c573202d57e9d7
Will Maier <wcmaier@m.aier.us>
2013-02-17 18:33:51 -0600

    Disable menu bar.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,4 +4,6 @@
 (global-set-key [remap eval-expression] 'pp-eval-expression)
 (global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
 
+(menu-bar-mode -1)
+
 (autoload 'magit-status' "magit" nil t)
