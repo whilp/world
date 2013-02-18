e82cd253ae5918bb380a6a6b47e1224123f0d366
Will Maier <wcmaier@m.aier.us>
2013-02-17 18:44:29 -0600

    Further decrapify emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,8 @@
 (global-set-key [remap eval-expression] 'pp-eval-expression)
 (global-set-key [remap eval-last-sexp] 'pp-eval-last-sexp)
 
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 (menu-bar-mode -1)
 
 (autoload 'magit-status' "magit" nil t)
