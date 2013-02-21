4cd01cab15a90cb955a49219299a2f20f90bcfff
Will Maier <wcmaier@m.aier.us>
2013-02-21 17:02:25 +0000

    Use hjkl for windmove.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,6 +58,10 @@
 
 ;; windows.
 (global-set-key (kbd "M-RET") 'ns-toggle-fullscreen)
+(global-set-key (kbd "M-h") 'windmove-left)
+(global-set-key (kbd "M-j") 'windmove-down)
+(global-set-key (kbd "M-k") 'windmove-up)
+(global-set-key (kbd "M-l") 'windmove-right)
 
 ;; s/meta/c/
 (global-set-key "\C-x\C-m" 'execute-extended-command)
