5f3f3f0e1739de6029d50e6785656223e4d1ce03
Will Maier <wcmaier@luft.lfod.us>
2013-04-28 18:51:48 +0000

    GIT_EDITOR.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,7 @@
 (setq explicit-shell-file-name "/bin/bash")
 (setenv "PAGER" "cat")
 (setenv "EDITOR" "emacsclient")
+(setenv "GIT_EDITOR" "emacsclient")
 (setenv "ALTERNATE_EDITOR" "emacs")
 (setenv "PROMPT_COMMAND" "")
 (setenv "GPG_AGENT_INFO" nil)
