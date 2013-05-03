2f5ab89ec8505b9726f1eeac9e970e5eacd56105
Will Maier <wcmaier@m.aier.us>
2013-05-03 16:43:32 +0000

    Further silence bell.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,7 +26,8 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
-(setq ring-bell-function 'ignore)
+(setq ring-bell-function 'ignore
+      visible-bell t)
 
 ;; modeline.
 (setq display-time-format "%a %Y-%m-%d %H:%M")
