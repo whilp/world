61bddef3e41a81663bb31c78712e651166d5635b
Will Maier <wcmaier@m.aier.us>
2013-12-21 03:40:53 +0000

    org-mode

---
+++ .emacs.d/el-get-init/init-org-mode.el
@@ -0,0 +1,15 @@
+(require 'org)
+(setq auto-indent-start-org-indent t
+      org-startup-indented t
+      org-use-tag-inheritance t
+      org-todo-keywords '((sequence "TODO" "DONE"))
+      org-agenda-restore-windows-after-quit t
+      org-agenda-search-headline-for-time nil
+      org-extend-today-until 6
+      org-agenda-start-with-follow-mode t
+      org-agenda-file-regexp "\\`[^.].*\\.\\(txt\\|org\\)\\'"
+      org-agenda-files (list "~/notes" "~/notes/projects" "~/notes/ops")
+      org-default-notes-file "~/notes/todo.txt")
+(define-key global-map (kbd "C-c a") 'org-agenda)
+(define-key global-map (kbd "C-c c") 'org-capture)
+(add-to-list 'auto-mode-alist '("\\.\\(txt\\|org\\)$" . org-mode))
