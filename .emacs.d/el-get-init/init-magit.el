b7d0d8805a39249c96d3c3ce792dea0f13495dd4
Will Maier <wcmaier@m.aier.us>
2013-12-30 16:39:46 +0000

    Never save magit buffers.

--- .emacs.d/el-get-init/init-magit.el
+++ .emacs.d/el-get-init/init-magit.el
@@ -1,7 +1,7 @@
 (autoload 'magit-status' "magit" nil t)
 (setq
  magit-git-executable "git"
- magit-save-some-buffers 'dontask
+ magit-save-some-buffers nil
  magit-status-buffer-switch-function 'switch-to-buffer
  )
 (define-key whilp/bindings-map (kbd "C-x C-g") 'magit-status)
