2ce4194bf9c1af60ec2f13b6c6a583535829ee43
Will Maier <wcmaier@m.aier.us>
2014-03-23 18:57:45 +0000

    Encrypt and keep the auth token.

--- .emacs.d/el-get-init/init-twittering-mode.el
+++ .emacs.d/el-get-init/init-twittering-mode.el
@@ -1,2 +1,3 @@
+(setq twittering-use-master-password t)
 (define-key twittering-mode-map (kbd "n") 'twittering-goto-next-status)
 (define-key twittering-mode-map (kbd "p") 'twittering-goto-previous-status)
