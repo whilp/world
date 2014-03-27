fdd406422ce7bf15f9bbd4a383a9fd96fae2d0d9
Will Maier <wcmaier@m.aier.us>
2014-03-27 06:16:38 +0000

    Hide org-indent from modeline

--- .emacs.d/el-get-init/init-delight.el
+++ .emacs.d/el-get-init/init-delight.el
@@ -2,4 +2,5 @@
 (delight '((golden-ratio-mode nil "golden-ratio")
            (visual-line-mode nil "simple")
            (magit-auto-revert-mode "" "magit")
+           (org-indent-mode "" "org-indent")
            (flyspell-mode nil "flyspell")))
