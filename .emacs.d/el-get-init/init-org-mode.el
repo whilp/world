ec7a60ccf76b7d00785f17d4a80ca11f0da7359d
Will Maier <wcmaier@m.aier.us>
2014-05-29 23:38:11 +0000

    Tweak org display.

--- .emacs.d/el-get-init/init-org-mode.el
+++ .emacs.d/el-get-init/init-org-mode.el
@@ -10,9 +10,11 @@
       org-todo-keywords '((sequence "TODO" "DONE"))
       org-agenda-skip-scheduled-if-done t
       org-agenda-restore-windows-after-quit t
+      org-agenda-window-setup 'current-window
       org-agenda-search-headline-for-time nil
       org-extend-today-until 6
-      org-agenda-start-with-follow-mode t
+      org-agenda-start-with-follow-mode nil
+      org-agenda-follow-mode nil
       org-agenda-file-regexp "\\`[^.].*\\.\\(txt\\|org\\)\\'"
       org-agenda-files '("~/notes/todo.txt")
       org-bookmark-names-plist '()
