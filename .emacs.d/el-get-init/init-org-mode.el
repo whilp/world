4697a8fd7c1eda21e0d355b9e9f93b213483eca2
Will Maier <wcmaier@m.aier.us>
2014-07-23 00:40:28 +0000

    Twiddle org

--- .emacs.d/el-get-init/init-org-mode.el
+++ .emacs.d/el-get-init/init-org-mode.el
@@ -9,18 +9,27 @@
       org-agenda-start-on-weekday 1
       org-todo-keywords '((sequence "TODO" "DONE"))
       org-agenda-skip-scheduled-if-done t
-      org-agenda-restore-windows-after-quit t
+      org-agenda-restore-windows-after-quit nil
       org-agenda-window-setup 'current-window
       org-agenda-search-headline-for-time nil
       org-extend-today-until 6
       org-agenda-start-with-follow-mode nil
+      org-agenda-todo-ignore-deadlines 'past
+      org-agenda-todo-ignore-with-date t
+      org-agenda-skip-scheduled-if-deadline-is-shown t
+      org-agenda-todo-ignore-timestamp 'all
+      org-agenda-todo-ignore-scheduled 'all
+      org-agenda-skip-deadline-if-done t
+      org-agenda-tags-todo-honor-ignore-options t
       org-agenda-follow-mode nil
       org-agenda-file-regexp "\\`[^.].*\\.\\(txt\\|org\\)\\'"
       org-agenda-files '("~/notes/todo.txt")
       org-bookmark-names-plist '()
       org-default-notes-file "~/notes/todo.txt"
       org-clock-mode-line-total 'today
-      org-clock-history-length 50)
+      org-clock-history-length 50
+      org-agenda-repeating-timestamp-show-all nil)
+
 (define-key global-map (kbd "C-c a") 'org-agenda)
 (define-key global-map (kbd "C-c c") 'org-capture)
 (define-key global-map (kbd "C-c C-x C-o") 'org-clock-out)
