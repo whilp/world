596cb0609a61abbc452204b03d6d412a386cc432
Will Maier <wcmaier@m.aier.us>
2014-07-23 00:39:20 +0000

    Go

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,7 @@
 (setq explicit-shell-file-name "/bin/bash")
 (setq exec-path (append '(
                           "~/bin"
+                          "~/go/bin"
                           "/usr/local/sbin"
                           "/usr/local/bin"
                           "/usr/local/MacGPG2/bin"
@@ -22,6 +23,8 @@
 (setenv "GIT_COMMITTER_EMAIL" "wcmaier@m.aier.us")
 (setenv "GIT_AUTHOR_NAME" "Will Maier")
 (setenv "GIT_AUTHOR_EMAIL" "wcmaier@m.aier.us")
+
+(setenv "GOPATH" (expand-file-name "~/go"))
 
 (define-minor-mode whilp/bindings "My global bindings" t nil (make-sparse-keymap))
 
