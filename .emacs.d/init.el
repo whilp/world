d0b73c9cf19f67748132ad7736b5e78ea1883c94
Will Maier <wcmaier@m.aier.us>
2014-08-13 20:24:06 +0000

    multi-occor-in-this-mode
    Thanks @whoahbot
    
    http://www.masteringemacs.org/articles/2011/07/20/searching-buffers-occur-mode/

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -108,6 +108,25 @@
 
 (define-key whilp/bindings-map (kbd "C-c C-o") 'occur)
 
+(eval-when-compile
+  (require 'cl))
+ 
+(defun get-buffers-matching-mode (mode)
+  "Returns a list of buffers where their major-mode is equal to MODE"
+  (let ((buffer-mode-matches '()))
+   (dolist (buf (buffer-list))
+     (with-current-buffer buf
+       (if (eq mode major-mode)
+           (add-to-list 'buffer-mode-matches buf))))
+   buffer-mode-matches))
+ 
+(defun multi-occur-in-this-mode ()
+  "Show all lines matching REGEXP in buffers with this major mode."
+  (interactive)
+  (multi-occur
+   (get-buffers-matching-mode major-mode)
+   (car (occur-read-primary-args))))
+
 ;; I'm an adult.
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
