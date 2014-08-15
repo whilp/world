2f0b8d4a3dc4936eda423e38e5e3103d4af39cfc
Will Maier <wcmaier@m.aier.us>
2014-08-15 20:16:03 +0000

    git-grep-in-root

--- .emacs.d/el-get-init/init-find-file-in-project.el
+++ .emacs.d/el-get-init/init-find-file-in-project.el
@@ -1,4 +1,10 @@
 (require 'find-file-in-project)
+
+(defun git-grep-in-root ()
+  (interactive)
+  (let ((default-directory (ffip-project-root)))
+    (call-interactively 'vc-git-grep)))
+
 (defun compile-in-root ()
   (interactive)
   (let ((default-directory (ffip-project-root)))
