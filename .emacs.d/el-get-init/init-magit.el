f737c591f22a22a7c83015cc069610215b5b67da
Will Maier <wcmaier@m.aier.us>
2014-10-27 00:39:38 +0000

    Always set upstream on push

--- .emacs.d/el-get-init/init-magit.el
+++ .emacs.d/el-get-init/init-magit.el
@@ -3,6 +3,7 @@
  magit-git-executable "gh"
  magit-save-some-buffers nil
  magit-status-buffer-switch-function 'switch-to-buffer
+ magit-set-upstream-on-push 'dontask
  )
 (define-key whilp/bindings-map (kbd "C-x C-g") 'magit-status)
 ;; (define-key whilp/bindings-map (kbd "C-c C-p") 'magit-push)
