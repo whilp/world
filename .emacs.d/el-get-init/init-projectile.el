084e7dbf6caa4fcbd430cfe742358ca8daaeac61
Will Maier <wcmaier@m.aier.us>
2014-10-27 01:52:41 +0000

    Default projectile-switch-project to vc (magit)

--- .emacs.d/el-get-init/init-projectile.el
+++ .emacs.d/el-get-init/init-projectile.el
@@ -1,7 +1,11 @@
 (projectile-global-mode)
 
-(setq projectile-mode-line '(
-                             :eval (format " [%s]" (projectile-project-name))))
+(setq
+ projectile-mode-line '(
+                        :eval (format " [%s]"
+                                      (projectile-project-name)))
+ projectile-switch-project-action 'projectile-vc
+ )
 (define-key whilp/bindings-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map [?\s-d] 'projectile-find-dir)
 (define-key projectile-mode-map [?\s-p] 'projectile-switch-project)
