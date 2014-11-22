8ec60eeec0f5ed61a457e4e8617c07e2ac2a467c
Will Maier <wcmaier@m.aier.us>
2014-11-22 20:20:48 +0000

    Make authinfo executable

--- .authinfo.py
+++ .authinfo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # https://gist.github.com/areina/3879626
-import re, os
+import re, os, sys
 
 def get_password_emacs(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
@@ -8,3 +8,5 @@ def get_password_emacs(machine, login, port):
     authinfo = os.popen("gpg -q --no-tty -d ~/.authinfo.gpg").read()
     return p.search(authinfo).group(1)
 
+if __name__ == "__main__":
+    print get_password_emacs(*sys.argv[1:])
