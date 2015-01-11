c0b35d79f54e2bbb30b9176306ec959043f9736f
Will Maier <wcmaier@m.aier.us>
2015-01-11 20:32:47 +0000

    Move znc setup to authinfo
    Add firrre, link to list of buffers from when I fucked everything up.

--- .emacs.d/el-get-init/init-znc.el
+++ .emacs.d/el-get-init/init-znc.el
@@ -1,10 +1,24 @@
+(require 'auth-source)
+
+(defun* my-znc-profile (name host user port)
+  (let* (
+         (auth-info
+          (car
+           (auth-source-search
+            :max 1
+            :host host
+            :user user
+            :port port
+            :create nil)))
+         (password (funcall (plist-get auth-info :secret))))
+    (list host port t (list (list name user password)))))
+
 (setq
  znc-erc-connector 'erc
  znc-servers
-   (quote
-    (("localhost" 12533 nil
-      ((freenode "freenode" "freenode")
-;;       (simple "simple" "simple")
-;;       (bitlbee "bitlbee" "simple")
-;;       (monitorama "monitorama" "simple")
-       )))))
+ (list
+  (my-znc-profile 'simple "chat.banksimple.com" "whilp" 9999)
+  (my-znc-profile 'furnance "furnace.firrre.com" "whilp" 9090)
+  ))
+
+;; buffers from the day i killed everything https://github.banksimple.com/gist/1c622769aee278dc2aad
