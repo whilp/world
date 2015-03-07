c23109f706271227d8df995be541ef07dc3b43b8
Will Maier <wcmaier@m.aier.us>
2015-03-07 23:32:17 +0000

    More OK themes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,8 +6,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
-(custom-set-faces)
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
