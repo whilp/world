bc240f2b18c9db4e3ff75255c88b469cdd66546b
Will Maier <wcmaier@m.aier.us>
2015-03-07 23:31:57 +0000

    Add boilerplate for custom-set-faces and custom-variables

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,19 @@
 ;; themes
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
 (custom-set-faces)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; environment.
 (setq explicit-shell-file-name "/bin/bash")
