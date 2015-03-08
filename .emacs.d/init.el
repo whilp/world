77ca367e576f6d876289ff20ef3ee61e83a84202
Will Maier <wcmaier@m.aier.us>
2015-03-08 00:26:17 +0000

    Rez customize bits for smart-mode-line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,19 @@
+;; customize
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; environment.
 (setq explicit-shell-file-name "/bin/bash")
 (setq exec-path
