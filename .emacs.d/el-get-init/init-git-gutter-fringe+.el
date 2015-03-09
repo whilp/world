eb8ff23ae064fccd5cd900f8b4463d854550a73a
Will Maier <wcmaier@m.aier.us>
2015-03-09 05:12:00 +0000

    git-gutter bindings

--- .emacs.d/el-get-init/init-git-gutter-fringe+.el
+++ .emacs.d/el-get-init/init-git-gutter-fringe+.el
@@ -16,3 +16,20 @@
 (define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-commit)
 (define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
 (define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer)
+(global-git-gutter+-mode t)
+(git-gutter-fr+-minimal)
+
+(define-key git-gutter+-mode-map (kbd "C-x n") 'git-gutter+-next-hunk)
+(define-key git-gutter+-mode-map (kbd "C-x p") 'git-gutter+-previous-hunk)
+
+;;; Act on hunks
+(define-key git-gutter+-mode-map (kbd "C-x v =") 'git-gutter+-show-hunk)
+(define-key git-gutter+-mode-map (kbd "C-x r") 'git-gutter+-revert-hunks)
+
+;; Stage hunk at point.
+;; If region is active, stage all hunk lines within the region.
+(define-key git-gutter+-mode-map (kbd "C-x t") 'git-gutter+-stage-hunks)
+(define-key git-gutter+-mode-map (kbd "C-x c") 'git-gutter+-commit)
+(define-key git-gutter+-mode-map (kbd "C-x C") 'git-gutter+-stage-and-commit)
+(define-key git-gutter+-mode-map (kbd "C-x C-y") 'git-gutter+-stage-and-commit-whole-buffer)
+(define-key git-gutter+-mode-map (kbd "C-x U") 'git-gutter+-unstage-whole-buffer)
