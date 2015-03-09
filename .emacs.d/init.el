4a46c3eabc2e461d4898ba823a1c918f4a51742a
Will Maier <wcmaier@m.aier.us>
2015-03-09 20:31:17 +0000

    Remove superfluous bits from mode line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -102,16 +102,13 @@
                 mode-line-front-space
                 mode-line-mule-info
                 mode-line-client
-                mode-line-modified
                 mode-line-remote
                 mode-line-frame-identification
                 mode-line-buffer-identification
-                "   " mode-line-position
                 (vc-mode vc-mode)
                 "  " mode-line-modes
                 mode-line-misc-info
-                mode-line-end-spaces)
-              global-mode-string '(""))
+                mode-line-end-spaces))
 
 ;; no splash.
 (setq inhibit-startup-message t
