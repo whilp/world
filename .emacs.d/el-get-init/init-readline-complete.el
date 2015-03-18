7fabdb87a85b19551ab53d2d854ffc8b2b05f7a1
Will Maier <wcmaier@m.aier.us>
2015-03-18 04:57:11 +0000

    Remove readline complete

--- .emacs.d/el-get-init/init-readline-complete.el
+++
@@ -1,6 +0,0 @@
-(require 'shell)
-(push 'company-readline company-backends)
-(add-hook 'rlc-no-readline-hook (lambda () (company-mode -1)))
-(setq explicit-shell-file-name "bash"
-      explicit-bash-args '("-c" "export EMACS=; stty echo; bash")
-      comint-process-echoes t)
