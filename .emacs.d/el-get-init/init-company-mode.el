c7c16901709af79d2e7b136eca9507d2a09426b7
Will Maier <wcmaier@m.aier.us>
2015-03-18 05:01:18 +0000

    Company mode tweaks, for go

--- .emacs.d/el-get-init/init-company-mode.el
+++ .emacs.d/el-get-init/init-company-mode.el
@@ -3,4 +3,5 @@
 (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
 (define-key company-active-map (kbd "<tab>") 'company-complete)
 (setq company-echo-delay 0
+      company-tooltip-minimum-width 30
       company-idle-delay .7)
--- .emacs.d/el-get-init/init-go-company.el
+++ .emacs.d/el-get-init/init-go-company.el
@@ -1,1 +1,5 @@
 (require 'company-go)
+(add-hook 'go-mode-hook 'company-mode)
+(add-hook 'go-mode-hook (lambda ()
+  (set (make-local-variable 'company-backends) '(company-go))
+  (company-mode)))
