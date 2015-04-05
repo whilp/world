2e69687208dcc62b92230a8c7bf11a90fc845291
Will Maier <wcmaier@m.aier.us>
2015-04-05 22:28:30 +0000

    Add my-toggle-solarized, bind to s-`

--- .emacs.d/el-get-init/init-color-theme-solarized.el
+++ .emacs.d/el-get-init/init-color-theme-solarized.el
@@ -1,3 +1,12 @@
+(require 'frame)
 (load-theme 'solarized t)
-(set-frame-parameter nil 'background-mode 'dark)
-(enable-theme 'solarized)
+
+(defun my-toggle-solarized ()
+  "Toggles between solarized light and dark"
+  (interactive)
+  (let ((mode (if (equal (frame-parameter nil 'background-mode) 'dark) 'light 'dark)))
+    (set-frame-parameter nil 'background-mode mode)
+    (enable-theme 'solarized)
+    (enable-theme 'solarized)))
+
+(define-key whilp/bindings-map (kbd "s-`") 'my-toggle-solarized)
