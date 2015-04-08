3ba2c49937a74c196b69e5aae2f3eecf1d6ed27a
Will Maier <wcmaier@gmail.com>
2015-04-08 06:32:03 +0000

    GC old el-get recipes

--- .emacs.d/el-get-recipes/ibuffer-projectile.el
+++
@@ -1,5 +0,0 @@
-(:name ibuffer-projectile
-       :type github
-       :pkgname "purcell/ibuffer-projectile"
-       :description "Group buffers in Emacs ibuffer-mode by their projectile root directory"
-       :website "https://github.com/purcell/ibuffer-projectile")
--- .emacs.d/el-get-recipes/rcirc-color.el
+++
@@ -1,6 +0,0 @@
-(:name rcirc-color
-       :type http
-       :description "color nicks"
-       :url "https://raw.githubusercontent.com/kensanata/elisp/master/rcirc-color.el"
-       :website "https://github.com/kensanata/elisp/blob/master/rcirc-color.el")
-
