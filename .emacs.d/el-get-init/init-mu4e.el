84d5f95204e43548e0ab577b6b1023b548ac84bc
Will Maier <wcmaier@gmail.com>
2015-04-18 20:48:40 +0000

    Use completing-read-function for mu4e

--- .emacs.d/el-get-init/init-mu4e.el
+++ .emacs.d/el-get-init/init-mu4e.el
@@ -12,6 +12,7 @@
  mu4e-sent-messages-behavior 'sent
  mu4e-compose-signature-auto-include nil
  mu4e-compose-complete-only-personal nil
+ mu4e-completing-read-function 'completing-read
  mu4e-compose-complete-ignore-address-regexp "\(no-?reply\|gitlab@gitlab\.com\|@docs\.google\.com\)"
  mu4e-compose-complete-only-after "2010-01-01"
  mu4e-compose-dont-reply-to-self t
