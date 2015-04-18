578304a59e446b7a8a7c24efb5ad3f96a853c44a
Will Maier <wcmaier@gmail.com>
2015-04-18 20:47:48 +0000

    Update custom variables

--- .emacs.d/lisp/whilp-ui.el
+++ .emacs.d/lisp/whilp-ui.el
@@ -10,6 +10,21 @@
 
 (bind-key "C-x C-c" nil)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (setq confirm-nonexistent-file-or-buffer nil
       display-time-default-load-average nil
       display-time-format "%a %Y-%m-%d %H:%M"
