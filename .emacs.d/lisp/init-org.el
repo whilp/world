1baa92c62a79e465e826d36745c6520b58ea33aa
Will Maier <wcmaier@gmail.com>
2015-05-04 00:02:10 +0000

    Twiddle org-mode
    - Organize bindings under s-o prefix
    - Add journal capture template

--- .emacs.d/lisp/init-org.el
+++ .emacs.d/lisp/init-org.el
@@ -10,6 +10,9 @@
 (eval-when-compile
   (require 'org))
 
+(bind-keys :prefix-map whilp-org-map
+           :prefix "s-o")
+
 (use-package org-plus-contrib
   :demand t
   :ensure t
@@ -31,11 +34,15 @@
 
 (use-package org-capture
   :demand t
-  :bind (("C-c c" . org-capture))
   :config
-  (setq org-capture-templates
-        '(("t" "Todo" entry (file "~/src/github.banksimple.com/whilp/notes/plan.txt")
-           "* TODO %?\nDEADLINE: %^t"))))
+  (progn
+    (bind-keys :map whilp-org-map
+               ("c" . org-capture))
+    (setq org-capture-templates
+          '(("j" "Journal" entry (file "~/src/github.banksimple.com/whilp/notes/journal.org")
+             "* %U %?")
+            ("t" "Todo" entry (file "~/src/github.banksimple.com/whilp/notes/plan.org")
+             "* TODO %?\nDEADLINE: %^t")))))
 
 (use-package org-context
   :ensure t
@@ -44,31 +51,35 @@
 
 (use-package org-agenda
   :demand t
-  :bind (("C-c a" . org-agenda))
   :config
-  (setq org-agenda-dim-blocked-tasks t
-        org-agenda-file-regexp "\\`[^.].*\\.\\(txt\\|org\\)\\'"
-        org-agenda-files '("~/src/github.banksimple.com/whilp/notes/plan.txt")
-        org-agenda-follow-mode nil
-        org-agenda-repeating-timestamp-show-all nil
-        org-agenda-restore-windows-after-quit nil
-        org-agenda-search-headline-for-time nil
-        org-agenda-skip-deadline-if-done t
-        org-agenda-skip-scheduled-if-deadline-is-shown t
-        org-agenda-skip-scheduled-if-done t
-        org-agenda-start-on-weekday 1
-        org-agenda-start-with-follow-mode nil
-        org-agenda-tags-todo-honor-ignore-options t
-        org-agenda-todo-ignore-deadlines 'past
-        org-agenda-todo-ignore-scheduled 'all
-        org-agenda-todo-ignore-timestamp 'all
-        org-agenda-todo-ignore-with-date t
-        org-agenda-window-setup 'current-window))
+  (progn
+    (bind-keys :map whilp-org-map
+               ("a" . org-agenda))
+    (setq org-agenda-dim-blocked-tasks t
+          org-agenda-file-regexp "\\`[^.].*\\.\\(txt\\|org\\)\\'"
+          org-agenda-files '("~/src/github.banksimple.com/whilp/notes/plan.txt")
+          org-agenda-follow-mode nil
+          org-agenda-repeating-timestamp-show-all nil
+          org-agenda-restore-windows-after-quit nil
+          org-agenda-search-headline-for-time nil
+          org-agenda-skip-deadline-if-done t
+          org-agenda-skip-scheduled-if-deadline-is-shown t
+          org-agenda-skip-scheduled-if-done t
+          org-agenda-start-on-weekday 1
+          org-agenda-start-with-follow-mode nil
+          org-agenda-tags-todo-honor-ignore-options t
+          org-agenda-todo-ignore-deadlines 'past
+          org-agenda-todo-ignore-scheduled 'all
+          org-agenda-todo-ignore-timestamp 'all
+          org-agenda-todo-ignore-with-date t
+          org-agenda-window-setup 'current-window)))
 
 (use-package org-clock
-  :bind (("C-c C-x C-o" . org-clock-out)
-         ("C-c C-x C-x" . org-clock-in-last)
-         ("C-c C-x C-i" . org-clock-in))
+  :init
+  (bind-keys :map whilp-org-map
+             ("o" . org-clock-out)
+             ("x" . org-clock-in-last)
+             ("i" . org-clock-in))
   :config
   (setq org-clock-history-length 50
         org-clock-mode-line-total 'today))
