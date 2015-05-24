8e608f10dc432af376415f609d584acda84c134f
Will Maier <wcmaier@gmail.com>
2015-05-24 07:55:58 +0000

    Add corral

--- .emacs.d/lisp/init-text.el
+++ .emacs.d/lisp/init-text.el
@@ -8,6 +8,16 @@
 
 (require 'use-package)
 
+(use-package corral
+  :ensure t
+  :bind (("M-9" . corral-parentheses-backward)
+         ("M-0" . corral-parentheses-forward)
+         ("M-[" . corral-brackets-backward)
+         ("M-]" . corral-brackets-forward)
+         ("M-{" . corral-braces-backward)
+         ("M-}" . corral-braces-forward)
+         ("M-\"" . corral-double-quotes-backward)))
+
 (use-package markdown-mode
   :ensure t
   :defer t
