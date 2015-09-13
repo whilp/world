0e06dcaa71fdbff63390615b8f30f751e64b8ca5
Will Maier <wcmaier@gmail.com>
2015-09-13 00:13:48 +0000

    Add a very terrible BC shim for helm-match-plugin

---
+++ .emacs.d/lisp/helm-match-plugin.el
@@ -0,0 +1,16 @@
+;;; helm-match-plugin --- BC-shim
+
+;;; Commentary:
+
+;;; BC
+
+;;; Code:
+
+;; this is terrible
+;; https://github.com/yasuyk/helm-company/pull/21
+
+(require 'helm-multi-match)
+
+
+(provide 'helm-match-plugin)
+;;; helm-match-plugin ends here
