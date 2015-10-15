91b0b248a11c4bae78edc88f4a584f5e5f56a93f
Will Maier <wcmaier@gmail.com>
2015-10-15 14:49:29 +0000

    Move projectile

--- .emacs.d/lisp/init-dev.el
+++ .emacs.d/lisp/init-dev.el
@@ -306,5 +306,64 @@
   :config
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
+(use-package projectile
+  :ensure t
+  :demand t
+  :config
+  (progn
+    (defun project-compilation-buffer (prefix)
+      (format "*%s: %s*" prefix (projectile-project-root)))
+    (defun with-compilation-buffer-name-function-for (prefix orig args)
+      (let* ((compilation-buffer-name-function
+              (lambda (name-of-mode)
+                (format "*%s: %s*" prefix (projectile-project-root)))))
+        (apply orig args)))
+
+    (defun with-compile-project (orig &rest args)
+      (with-compilation-buffer-name-function-for "compile-project" orig args))
+    (defun with-test-project (orig &rest args)
+      (with-compilation-buffer-name-function-for "test-project" orig args))
+
+    (advice-add 'projectile-compile-project
+                :around #'with-compile-project)
+    (advice-add 'projectile-test-project
+                :around #'with-test-project)
+
+    (defun projectile-ignore-project (name)
+      "Return nil if project NAME should be ignored."
+      (string-match
+       (rx
+        (or "/homebrew/"
+            "/.emacs.d/"))
+       name))
+    (defun projectile-run-shell (&optional buffer)
+      "Start a shell in the project's root."
+      (interactive "P")
+      (projectile-with-default-dir (projectile-project-root)
+        (shell (format "*shell %s*" (projectile-project-name)))))
+    (bind-keys :map projectile-command-map
+               ("!" . projectile-run-shell)
+               ("i" . projectile-compile-project)
+               ("o" . projectile-test-project))
+    (setq projectile-keymap-prefix (kbd "C-c p")
+          projectile-ignored-project-function 'projectile-ignore-project
+          projectile-globally-ignored-directories
+          (quote (".idea"
+                  ".eunit"
+                  ".git"
+                  ".hg"
+                  ".fslckout"
+                  ".bzr"
+                  "_darcs"
+                  ".tox"
+                  ".svn"
+                  "build"
+                  "_workspace"))
+          projectile-mode-line
+          (quote
+           (:eval (format " [%s]" (projectile-project-name)))))
+    (define-key projectile-mode-map projectile-keymap-prefix 'projectile-command-map)
+    (projectile-global-mode)))
+
 (provide 'init-dev)
 ;;; init-dev ends here
