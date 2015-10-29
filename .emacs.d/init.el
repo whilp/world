db9c2cc44890ab007b79da16a4c13239b708a917
Will Maier <wcmaier@gmail.com>
2015-10-29 20:56:20 +0000

    Update custom vars

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,7 +66,7 @@
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(package-selected-packages
    (quote
-    (counsel ivy pandoc-mode browse-at-remote package-build shut-up epl git commander f dash s))))
+    (company-go pkg paredit-everywhere counsel ivy pandoc-mode browse-at-remote package-build shut-up epl git commander f dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
