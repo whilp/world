689fb7c9cf2b3aa97eaecd0e5f9e08d9aabc24cb
Will Maier <wcmaier@gmail.com>
2015-11-11 23:48:33 +0000

    Sort emacs packages

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -13,59 +13,59 @@
           # ace-jump-zap
           # ace-link
           # ace-window
-          async
           # auto-indent-mode
-          avy
           # browse-at-remote
           # clojure-mode
-          colorThemeSolarized
           # comment-dwim-2
-          company
           # company-go
           # company-nixos-options
           # corral
           # csv-mode
           # csv-nav
-          deferred
           # diff-hl
           # dockerfile-mode
           # easy-kill
           # edit-server-htmlize
           # eval-in-repl
+          # go-eldoc
+          # go-mode
+          # go-oracle
+          # graphviz-dot-mode
+          # guru-mode
+          # json-mode
+          # lua-mode
+          # nix-mode
+          # ob-ipython
+          # paredit
+          # paredit-everywhere
+          # pdf-tools
+          # rcirc-color
+          # rcirc-controls
+          # restclient
+          # unfill
+          # which-key
+          # yaml-mode
+          async
+          avy
+          colorThemeSolarized
+          company
+          deferred
           expand-region
           flycheck
           gh
           gist
           gitModes
           htmlize
-          # go-eldoc
-          # go-mode
-          # go-oracle
-          # graphviz-dot-mode
-          # guru-mode
-          swiper # swiper ivy counsel
           js2
-          # json-mode
-          # lua-mode
           magit
           markdown-mode
-          # nix-mode
-          # ob-ipython
           org
           org-plus-contrib
-          # paredit
-          # paredit-everywhere
-          # pdf-tools
           projectile
-          # rcirc-color
-          # rcirc-controls
-          # restclient
           scalaMode2
           smart-mode-line
-          # unfill
+          swiper
           use-package
-          # which-key
-          # yaml-mode
       ]))
       git
       gitAndTools.hub
