d5c2a72d6083f9451ea9fd335c35ccbca5fde479
Will Maier <wcmaier@gmail.com>
2016-01-01 21:51:17 +0000

    Add whilp-git, whilp-projectile

---
+++ .spacemacs.d/whilp-git/config.el
@@ -0,0 +1,44 @@
+(require 'magit)
+
+(magit-define-popup-action
+  'magit-dispatch-popup ?H "Github" 'magit-github-popup)
+
+(magit-define-popup magit-github-popup
+  "Popup console for Github interaction."
+  :actions  '((?p "Pull request" magit-pull-request)
+              (?f "Fork" magit-fork))
+  :default-action 'magit-pull-request)
+
+(defun magit-pull-request ()
+  "Open a Github pull request.
+
+Run `hub pull-request' asynchronously; see
+`magit-pull-request-sentinel' for the interesting bits."
+  (interactive)
+  (set-process-sentinel
+   (let ((magit-git-executable "hub"))
+     (magit-run-git-with-editor "pull-request")
+     'magit-pull-request-sentinel)))
+
+(defun magit-pull-request-sentinel (process event)
+  "Handle EVENT in PROCESS.
+
+After `hub pull-request' exits, check the output logged in the magit
+process log for a pattern that looks like a pull request URL and add
+it to the kill ring."
+  (magit-process-sentinel process event)
+  (when (eq (process-status process) 'exit)
+    (with-current-buffer (process-buffer process)
+      (let* ((section (process-get process 'section))
+             (beg (marker-position (magit-section-content section)))
+             (end (marker-position (magit-section-end section)))
+             (content (buffer-substring beg end))
+             (match (string-match "http.*/pull/.*" content))
+             (url (match-string 0 content)))
+        (kill-new url)))))
+
+(defun magit-fork ()
+  "Fork a Github repository."
+  (interactive)
+  (let ((magit-git-executable "hub"))
+    (magit-run-git-async "fork")))
---
+++ .spacemacs.d/whilp-projectile/config.el
@@ -0,0 +1,39 @@
+(spacemacs|use-package-add-hook projectile
+  :post-init
+  (progn
+    (helm-projectile-define-key helm-projectile-projects-map
+      (kbd  "C-s") #'spacemacs/helm-project-smart-do-search)
+    (helm-projectile-define-key helm-projectile-find-file-map
+      (kbd "C-s") #'spacemacs/helm-project-smart-do-search)
+    (bind-keys :map projectile-command-map
+               ("!" . projectile-run-shell)
+               ("i" . projectile-compile-project)
+               ("o" . projectile-test-project)
+               ("g" . helm-projectile-grep)))
+  :post-config
+  (setq projectile-completion-system 'helm
+        projectile-mode-line
+        (quote
+         (:eval (format " [%s]" (projectile-project-name))))))
+
+(defun projectile-run-shell (&optional buffer)
+  "Start a shell in the project's root (ignoring BUFFER)."
+  (interactive "P")
+  (projectile-with-default-dir (projectile-project-root)
+    (let ((eshell-buffer-name (format "*shell %s*" (projectile-project-name))))
+      (eshell))))
+
+(defun with-compilation-buffer-name-function-for (prefix orig args)
+  "With a compilation buffer name beginning with PREFIX, apply ORIG and ARGS."
+  (let* ((compilation-buffer-name-function
+          (lambda (name-of-mode)
+            (format "*%s: %s*" prefix (projectile-project-root)))))
+    (apply orig args)))
+
+(defun with-compile-project (orig &rest args)
+  "Wrap ORIG with ARGS to compile a project in a dedicated buffer."
+  (with-compilation-buffer-name-function-for "compile-project" orig args))
+
+(defun with-test-project (orig &rest args)
+  "Wrap ORIG with ARGS to test a project in a dedicated buffer."
+  (with-compilation-buffer-name-function-for "test-project" orig args))
