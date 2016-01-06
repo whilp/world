d6ae681291df0269b25181765f527433d04341af
Will Maier <wcmaier@gmail.com>
2016-01-06 06:01:26 +0000

    Spacemacs-ify magit config

--- .spacemacs.d/whilp-git/packages.el
+++ .spacemacs.d/whilp-git/packages.el
@@ -14,7 +14,7 @@
 ;; which require an initialization must be listed explicitly in the list.
 (setq whilp-git-packages
     '(
-      ;; package names go here
+      magit
       ))
 
 ;; List of packages to exclude.
@@ -29,3 +29,48 @@
 ;; Often the body of an initialize function uses `use-package'
 ;; For more info on `use-package', see readme:
 ;; https://github.com/jwiegley/use-package
+
+(defun whilp-git/pre-init-magit ()
+  (require 'magit-popup)
+  (magit-define-popup-action
+    'magit-dispatch-popup ?H "Github" 'magit-github-popup)
+
+  (magit-define-popup magit-github-popup
+    "Popup console for Github interaction."
+    :actions  '((?p "Pull request" magit-pull-request)
+                (?f "Fork" magit-fork))
+    :default-action 'magit-pull-request))
+
+(defun magit-pull-request ()
+  "Open a Github pull request.
+
+Run `hub pull-request' asynchronously; see
+`magit-pull-request-sentinel' for the interesting bits."
+  (interactive)
+  (let ((magit-git-executable "hub"))
+    (set-process-sentinel
+     (magit-run-git-with-editor "pull-request")
+     'magit-pull-request-sentinel)))
+
+(defun magit-pull-request-sentinel (process event)
+  "Handle EVENT in PROCESS.
+
+After `hub pull-request' exits, check the output logged in the magit
+process log for a pattern that looks like a pull request URL and add
+it to the kill ring."
+  (magit-process-sentinel process event)
+  (when (eq (process-status process) 'exit)
+    (with-current-buffer (process-buffer process)
+      (let* ((section (process-get process 'section))
+             (beg (marker-position (magit-section-content section)))
+             (end (marker-position (magit-section-end section)))
+             (content (buffer-substring beg end))
+             (match (string-match "http.*/pull/.*" content))
+             (url (match-string 0 content)))
+        (kill-new url)))))
+
+(defun magit-fork ()
+  "Fork a Github repository."
+  (interactive)
+  (let ((magit-git-executable "hub"))
+    (magit-run-git-async "fork")))
