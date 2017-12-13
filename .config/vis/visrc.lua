1d40c82c45d873b0bd632f4ce46b0a384cda4e26
Will Maier <wcmaier@gmail.com>
2017-12-13 20:13:03 +0000

    Start vis config

---
+++ .config/vis/visrc.lua
@@ -0,0 +1,19 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+require('plugins/complete-filename')
+require('plugins/complete-word')
+require('plugins/filetype')
+require('plugins/textobject-lexer')
+
+
+vis.events.subscribe(vis.events.INIT, function()
+	vis:command('set theme solarized')
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	vis:command('set number')
+	vis:command('set autoindent')
+	vis:command('set colorcolumn 80')
+	vis:command('set show-tabs')
+end)
+
