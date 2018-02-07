0bff1ca5b79d26ce48a5717cb5a3ab502eace57e
Will Maier <wcmaier@gmail.com>
2018-02-07 22:19:45 +0000

    Add python PATHs, make PATH deterministic

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,21 @@
 fish_hybrid_key_bindings
 
-set CDPATH . ~/src/* $CDPATH
+set -x LANG en_US.UTF-8
 
-set PATH ~/bin $PATH
+set -x CDPATH . ~/src/* $CDPATH
 
+set -x PATH /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
+
+# Go.
 set -x GOPATH $HOME
 
-set JAVA_HOME /usr/lib/jvm/(update-java-alternatives --list | cut -f1 -d\  | head -1 2>/dev/null)
+# Java.
+set -x JAVA_HOME /usr/lib/jvm/(update-java-alternatives --list | cut -f1 -d\  | head -1 2>/dev/null)
 set PATH $JAVA_HOME/bin $PATH
 
+# Python.
+set PATH ~/.local/bin $PATH
+set PATH ~/.python/bin $PATH
+
+# Me.
+set PATH ~/bin $PATH
