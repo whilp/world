e6208ad768fb6d86e7add5ab5d44f92bc4309476
Will Maier <wcmaier@gmail.com>
2018-02-07 22:04:04 +0000

    Set JAVA_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,9 @@ fish_hybrid_key_bindings
 set CDPATH . ~/src/* $CDPATH
 
 set PATH ~/bin $PATH
-set PATH /usr/lib/jvm/default-jvm/bin $PATH
 
 set -x GOPATH $HOME
+
+set JAVA_HOME /usr/lib/jvm/(update-java-alternatives --list | cut -f1 -d\  | head -1 2>/dev/null)
+set PATH $JAVA_HOME/bin $PATH
 
