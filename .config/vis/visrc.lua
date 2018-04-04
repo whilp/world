023f209a3499639a833416ca743a11f5a9c90a0c
Will Maier <wcmaier@gmail.com>
2018-04-04 20:34:54 +0000

    Support py, lua styles

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -7,14 +7,38 @@ require('plugins/textobject-lexer')
 
 
 vis.events.subscribe(vis.events.INIT, function()
-	vis:command('set theme solarized')
+    vis:command('set theme solarized')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command('set number')
-	vis:command('set autoindent')
-	vis:command('set colorcolumn 80')
-	vis:command('set show-tabs')
-	vis:command('set horizon 128')
+    vis:command('set number')
+    vis:command('set autoindent')
+    vis:command('set colorcolumn 80')
+    vis:command('set show-tabs')
+    vis:command('set horizon 128')
 end)
 
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+    local exts = {
+        lua = function()
+            vis:command("set tw 4")
+            vis:command("set et")
+            vis:command("set ai")
+        end,
+        py = function()
+            vis:command("set ai")
+            vis:command("set et")
+            vis:command("set tw 4")
+        end,
+    }
+    fn = exts[ext(win.file.name)]
+    if fn ~= nil then fn() end
+end)
+
+ext = function(filename)
+    if filename == nil then return nil end
+    got = filename:find("%.%S+$")
+    if got == nil then return nil end
+    i, j = got
+    return filename:sub(i+1, j)
+end
