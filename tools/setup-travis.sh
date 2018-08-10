f86c02e073afbca2cfa11112f71b0eed8f1c9ee8
Will Maier <wcmaier@gmail.com>
2018-08-10 23:24:06 +0000

    Refactor setup

--- tools/setup-travis.sh
+++ tools/setup-travis.sh
@@ -3,6 +3,19 @@
 set -euo pipefail
 
 main() {
+	os="$1"
+	case "$os" in
+	linux) setup_linux ;;
+	osx) setup_osx ;;
+	esac
+	return $?
+}
+
+setup_linux() {
+	return 0
+}
+
+setup_osx() {
 	brew update
 	brew install openssl readline
 	brew outdated pyenv || brew upgrade pyenv
@@ -10,10 +23,11 @@ main() {
 	export PYENV_VERSION=$PYTHON
 	export PATH="$HOME/.pyenv/shims:${PATH}"
 	pyenv-virtualenv venv
-	source venv/bin/activate
+	source ./venv/bin/activate
 
 	python -m pip install -U pip
 	python -m easy_install -U setuptools
+	return 0
 }
 
 main "$@"
