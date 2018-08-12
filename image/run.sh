a7cbcb1da5f10c00a5861bb74b32c82d48f7138a
Will Maier <wcmaier@gmail.com>
2018-08-12 02:51:32 +0000

    Format

--- image/run.sh
+++ image/run.sh
@@ -4,26 +4,26 @@ set -euo pipefail
 
 clipper_pid=
 
-main () {
-    #clipper="$1"
-    
-    # Listen on localhost:8377; mounting a socket into the container
-    # doesn't work. https://github.com/docker/for-mac/issues/483
-    #$clipper &
-    #clipper_pid=$!
+main() {
+	#clipper="$1"
 
-    docker run -ti \
-        -v ~/src:/src:cached \
-        -v cache:/cache:cached \
-        -v /var/run/docker.sock:/var/run/docker.sock \
-	"$@" \
-        bazel/image:image
-    return $?
+	# Listen on localhost:8377; mounting a socket into the container
+	# doesn't work. https://github.com/docker/for-mac/issues/483
+	#$clipper &
+	#clipper_pid=$!
+
+	docker run -ti \
+		-v ~/src:/src:cached \
+		-v cache:/cache:cached \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		"$@" \
+		bazel/image:image
+	return $?
 }
 
-clean () {
-    #kill -9 "$clipper_pid"
-    true
+clean() {
+	#kill -9 "$clipper_pid"
+	true
 }
 trap clean EXIT
 
