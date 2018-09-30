b82eb185e30b9edf42aa54bc4ff7b18c9a7a7274
Will Maier <wcmaier@gmail.com>
2018-09-30 00:43:59 +0000

    run-bazel: make invocation more clear

--- tools/run-bazel.sh
+++ tools/run-bazel.sh
@@ -5,15 +5,18 @@ set -euo pipefail
 main() {
 	bazel="$1"
 	shift
-	$bazel \
-		--output_base="$HOME/.cache/bazel" \
-		--host_jvm_args=-Xmx500m \
-		--host_jvm_args=-Xms500m \
-		test \
-		--config=ci \
-		--experimental_repository_cache="$HOME/.bazel_repository_cache" \
-		--local_resources=400,1,1.0 \
-		"$@"
+	(
+		set -x
+		$bazel \
+			--output_base="$HOME/.cache/bazel" \
+			--host_jvm_args=-Xmx500m \
+			--host_jvm_args=-Xms500m \
+			test \
+			--config=ci \
+			--experimental_repository_cache="$HOME/.bazel_repository_cache" \
+			--local_resources=400,1,1.0 \
+			"$@"
+	)
 	return $?
 }
 
