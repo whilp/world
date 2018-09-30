eb945c1784d8c07d8d7505443366bc43fd9776a8
Will Maier <wcmaier@gmail.com>
2018-09-30 13:37:33 +0000

    tools/run-bazel: whitespace

--- tools/run-bazel.sh
+++ tools/run-bazel.sh
@@ -3,21 +3,21 @@
 set -euo pipefail
 
 main() {
-	bazel="$1"
-	shift
-	(
-		set -x
-		$bazel \
-			--output_base="$HOME/.cache/bazel" \
-			--host_jvm_args=-Xmx500m \
-			--host_jvm_args=-Xms500m \
-			test \
-			--config=ci \
-			--experimental_repository_cache="$HOME/.bazel_repository_cache" \
-			--local_resources=400,1,1.0 \
-			"$@"
-	)
-	return $?
+    bazel="$1"
+    shift
+    (
+        set -x
+        $bazel \
+            --output_base="$HOME/.cache/bazel" \
+            --host_jvm_args=-Xmx500m \
+            --host_jvm_args=-Xms500m \
+            test \
+            --config=ci \
+            --experimental_repository_cache="$HOME/.bazel_repository_cache" \
+            --local_resources=400,1,1.0 \
+            "$@"
+    )
+    return $?
 }
 
 main "$@"
