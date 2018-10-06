31c4c79dac01970d94d37a92e16373b9326f128e
Will Maier <wcmaier@gmail.com>
2018-10-06 20:18:57 +0000

    tools/run-bazel: remove flags
    Try to make Travis work

--- tools/run-bazel.sh
+++ tools/run-bazel.sh
@@ -11,12 +11,8 @@ main() {
         set -x
         $bazel \
             --output_base="$HOME/.cache/bazel" \
-            --host_jvm_args=-Xmx500m \
-            --host_jvm_args=-Xms500m \
             test \
             --config=ci \
-            --experimental_repository_cache="$HOME/.bazel_repository_cache" \
-            --local_resources=400,1,1.0 \
             "$@"
     )
     return $?
