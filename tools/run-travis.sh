72ac98fe6711cf1d219a8db8b802a7d4b0ab9eaa
Will Maier <wcmaier@gmail.com>
2018-10-08 17:34:19 +0000

    strip path

--- tools/run-travis.sh
+++ tools/run-travis.sh
@@ -5,9 +5,24 @@ set -euo pipefail
 main() {
     bin="$1"
     set -x
-    env
-    command -v python
-    PATH="$bin:$PATH" ./tools/run-bazel.sh "$@"
+    bins=(
+        "$bin"
+        #/usr/local/clang-5.0.0/bin
+        /usr/local/sbin
+        /usr/local/bin
+        /usr/sbin
+        /usr/bin
+        /sbin
+        /bin
+    )
+    path="$(join : "${bins[@]}")"
+    PATH="$path" ./tools/run-bazel.sh "$@"
+}
+
+join() {
+    local IFS="$1"
+    shift
+    echo "$*"
 }
 
 main "$@"
