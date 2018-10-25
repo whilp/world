9b94fb33ddb7e21e11d009e631c5b519827a0e72
Will Maier <wcmaier@gmail.com>
2018-10-25 21:09:06 +0000

    image: add a test
    TODO

--- image/test.sh
+++ image/test.sh
@@ -3,7 +3,19 @@
 set -euo pipefail
 
 main() {
+    #test_image
+    ./image/check
+    return 0
+}
+
+test_image() {
+    # Load the image.
     ./image/image
+
+    iid="$(cat ./image/image.digest)"
+
+    docker run --rm -t --entrypoint=/check "$iid"
+
     return $?
 }
 
