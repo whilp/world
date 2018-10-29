ab9bfc441b9d7690f98be118f3da098adc36fb08
Will Maier <wcmaier@gmail.com>
2018-10-29 03:51:16 +0000

    cmd/3p: split main

---
+++ cmd/3p/github.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type githubArchive struct {
+	Name   string `json:"name"`
+	Owner  string `json:"owner"`
+	Ref    string `json:"ref"`
+	Repo   string `json:"repo"`
+	Sha256 string `json:"sha256"`
+}
+
+func (g githubArchive) ruleName() string {
+	return g.Name
+}
+
+// TODO: would be nice to support multiple URLs.
+func (g githubArchive) url() string {
+	return fmt.Sprintf("https://github.com/%s/%s/archive/%s.tar.gz", g.Owner, g.repo(), g.Ref)
+}
+
+func (g githubArchive) stripPrefix() string {
+	return fmt.Sprintf("%s-%s", g.repo(), g.Ref)
+}
+
+func (g githubArchive) repo() string {
+	if g.Repo == "" {
+		return g.Name
+	}
+	return g.Repo
+}
+
+func (g githubArchive) Rule() rule {
+	return rule{
+		Name: "http_archive",
+		Parameters: map[string]string{
+			"name":         g.ruleName(),
+			"url":          g.url(),
+			"strip_prefix": g.stripPrefix(),
+			"sha256":       g.Sha256,
+		},
+	}
+}
