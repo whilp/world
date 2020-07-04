51b53d3f1acecf7742a08d59c3b836b2d09d7a1d
Will Maier <189851+whilp@users.noreply.github.com>
2020-07-04 17:26:03 +0000

    fix: get rootless docker working

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -4,8 +4,16 @@ set -euo pipefail
 
 main() {
   set -x
-  sudo mkdir -p /home/user
-  sudo chown user:user /home/user
+  home="/home/user"
+  export XDG_RUNTIME_DIR="$home/.xdg"
+  export DOCKER_HOST="unix:///$XDG_RUNTIME_DIR/docker.sock"
+
+  sudo mkdir -p "$home"
+  sudo mkdir -p "$XDG_RUNTIME_DIR"
+  sudo chown -R user:user "$home"
+
+  dockerd-rootless.sh --experimental >"$XDG_RUNTIME_DIR/docker.log" 2>&1 &
+
   #sleep infinity
   exec "$@"
 }
