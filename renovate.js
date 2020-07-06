682ed4cd3d8e991fdf1c5b16940e2bdc3dd3b897
Will Maier <189851+whilp@users.noreply.github.com>
2020-07-06 05:00:43 +0000

    renovate actions

--- renovate.js
+++ renovate.js
@@ -30,12 +30,19 @@ module.exports = {
       datasourceTemplate: "docker",
     },
     {
-      fileMatch: [".github/workflow/.*.yml"],
+      fileMatch: ["^\\.github/workflows/[^/]+\\.ya?ml$"],
       labels: ["dependencies", "docker"],
       matchStrings: [
         "docker://(?<depName>.*?):(?<currentValue>.*?)@(?<currentDigest>sha256:[a-f0-9]+)",
       ],
       datasourceTemplate: "docker",
+    },
+    {
+      // https://github.com/renovatebot/renovate/issues/5733
+      fileMatch: ["^\\.github/workflows/[^/]+\\.ya?ml$"],
+      labels: ["dependencies", "github"],
+      matchStrings: ["uses: (?<depName>.*?)@(?<currentValue>.*?)\\s"],
+      datasourceTemplate: "github-tags",
     },
     {
       fileMatch: "versions.bzl",
