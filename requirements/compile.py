07eb8826c6ae9765581345d8a65f5106d293d445
Will Maier <189851+whilp@users.noreply.github.com>
2020-07-06 18:03:48 +0000

    set temporary cache

--- requirements/compile.py
+++ requirements/compile.py
@@ -2,16 +2,26 @@
 
 import os
 import sys
+import tempfile
 
 
 def main(args, env):
-    output_file = os.path.join(env.get("BUILD_WORKSPACE_DIRECTORY", "."), "requirements.txt")
+    output_file = os.path.abspath(os.path.join(env.get("BUILD_WORKSPACE_DIRECTORY", "."), "requirements.txt"))
     os.environ.update(env, CUSTOM_COMPILE_COMMAND="bazel run requirements:compile")
     extra = [f"--output-file={output_file}", "--generate-hashes", "--allow-unsafe", "--no-header"]
 
+    with tempfile.TemporaryDirectory() as tmp:
+        os.environ["XDG_CACHE_HOME"] = os.path.join(tmp, "cache")
+        return run(args[1:] + extra, env)
+
+
+def run(args, env):
     import piptools.scripts.compile as pip_compile
 
-    pip_compile.cli(args[1:] + extra)
+    try:
+        return pip_compile.cli(args)
+    except SystemExit:
+        pass
 
 
 if __name__ == "__main__":
