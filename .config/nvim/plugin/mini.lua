1a521d4e47a08321e4637d38bfb66254c98440f7
Will Maier <189851+whilp@users.noreply.github.com>
2025-09-20 16:06:47 -0700

    nvim: more mini

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -4,12 +4,9 @@ vim.pack.add({
 
 local ok, mini = pcall(require, "mini")
 if ok then
-  -- Initialize mini.nvim modules as needed
-  -- Example: mini.statusline.setup()
-  -- Example: mini.tabline.setup()
-  -- Example: mini.pairs.setup()
   require("mini.bufremove").setup()
   require("mini.diff").setup()
   require("mini.git").setup()
   require("mini.pick").setup()
+  require("mini.extra").setup()
 end
