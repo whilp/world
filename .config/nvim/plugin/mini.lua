76e0fb7ab604af43335958d86e1a474efe08b155
Will Maier <189851+whilp@users.noreply.github.com>
2025-10-09 10:51:13 -0700

    nvim: pick bindings

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -13,4 +13,12 @@ if ok then
     background = '#0d1117',
     foreground = '#c9d1d9',
   })
+
+  -- mini.pick keybindings
+  vim.keymap.set('n', '<Space>pf', '<Cmd>Pick files<CR>', { desc = 'Pick files' })
+  vim.keymap.set('n', '<Space>pg', '<Cmd>Pick git_files<CR>', { desc = 'Pick git files' })
+  vim.keymap.set('n', '<Space>pb', '<Cmd>Pick buffers<CR>', { desc = 'Pick buffers' })
+  vim.keymap.set('n', '<Space>ph', '<Cmd>Pick help<CR>', { desc = 'Pick help' })
+  vim.keymap.set('n', '<Space>pr', '<Cmd>Pick resume<CR>', { desc = 'Resume last pick' })
+  vim.keymap.set('n', '<Space>p/', '<Cmd>Pick grep_live<CR>', { desc = 'Live grep' })
 end
