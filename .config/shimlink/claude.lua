1e575631c41c1a48a00b351007d51d3738054144
Will Maier <189851+whilp@users.noreply.github.com>
2025-11-30 17:51:07 +0000

    shimlink: use non-musl claude binary for linux-x64

--- .config/shimlink/claude.lua
+++ .config/shimlink/claude.lua
@@ -10,8 +10,8 @@ Version{
       sha256 = "6474f28bfaaa28ae32c0a526ca8ba00364cb4fc3eea442f8826221f24c8eccb1"
     },
     ["linux-x86_64"] = {
-      arch = "linux-x64-musl",
-      sha256 = "8e80ae6bd5837fca033922dd77f7c9eb41ec907e9923fea00abf631107b94106"
+      arch = "linux-x64",
+      sha256 = "49de548ec2e5e994c4c56b0b47060c4c2bf06fefd7f85889b951518aa84cfa05"
     }
   },
   url = "https://storage.googleapis.com/claude-code-dist-86c565f3-f756-42ad-8dfa-d59b1c096819/claude-code-releases/${version}/${arch}/${name}",
