a88978ee7a7dea591e153c0bfdcc04cc94895f05
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-20 16:48:44 +0000

    make: add landlock sandboxing for test execution
    Add .UNVEIL rules to all test targets:
    - test-3p-lua: sandbox to 3p/lua, lua binary, test runner, output dir
    - test-lib-whereami: sandbox to .local/lib/lua, lua binary, test runner, CURDIR
    - test-work: sandbox to .local/lib/lua, bootstrap lib (for posix.signal.so), lua binary, test runner, work test dir
    - test-home: sandbox to home, .local/lib/lua, lua binary, test runner, home/o
    
    All passing tests verified to work with sandboxing enabled.
    Note: test-work has pre-existing failure (dynamic library issue in fat lua binary).

--- test.mk
+++ test.mk
@@ -1,17 +1,44 @@
 test_runner := .local/lib/lua/run-test.lua
 
+test-3p-lua: private .UNVEIL = \
+	r:3p/lua \
+	rx:$(lua_bin) \
+	r:$(test_runner) \
+	rwc:3p/lua/o \
+	rw:/dev/null
 test-3p-lua: lua
 	cd 3p/lua && $(CURDIR)/$(lua_bin) $(CURDIR)/$(test_runner) test.lua
 
+test-lib-whereami: private .UNVEIL = \
+	r:.local/lib/lua \
+	rx:$(lua_bin) \
+	r:$(test_runner) \
+	r:$(CURDIR) \
+	rw:/dev/null
 test-lib-whereami: lua
 	cd .local/lib/lua && HOME=$(CURDIR) LUA_PATH="$(CURDIR)/.local/lib/lua/?.lua;;" \
 		$(CURDIR)/$(lua_bin) $(CURDIR)/$(test_runner) test_whereami.lua
 
+test-work: private .UNVEIL = \
+	r:.local/lib/lua \
+	r:/home/codespace/.local/bootstrap/lib \
+	rx:$(lua_bin) \
+	r:$(test_runner) \
+	r:$(CURDIR) \
+	rwc:.local/lib/lua/test/work \
+	rw:/dev/null
 test-work: lua
 	cd .local/lib/lua/test/work && HOME=$(CURDIR) \
 		LUA_PATH="$(CURDIR)/.local/lib/lua/?.lua;$(CURDIR)/.local/lib/lua/test/work/?.lua;;" \
 		$(CURDIR)/$(lua_bin) $(CURDIR)/$(test_runner) run.lua
 
+test-home: private .UNVEIL = \
+	r:home \
+	r:.local/lib/lua \
+	rx:$(lua_bin) \
+	r:$(test_runner) \
+	rwc:home/o \
+	rw:/dev/null
 test-home: lua
 	cd home && LUA_PATH="$(CURDIR)/.local/lib/lua/?.lua;;" \
 		$(CURDIR)/$(lua_bin) $(CURDIR)/$(test_runner) test_main.lua
