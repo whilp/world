2fe4284395abf5d8b0475431320e4620cdc8d018
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-26 20:47:03 +0000

    cosmos: update to fat APE release 2025.12.26-0ce9031da

--- 3p/cosmos/cook.mk
+++ 3p/cosmos/cook.mk
@@ -1,12 +1,12 @@
 # cosmos binaries from whilp/cosmopolitan fork
 cosmos_dir := $(3p)/cosmos
-cosmos_version := 2025.12.26-564e3b5b9
+cosmos_version := 2025.12.26-0ce9031da
 cosmos_url := https://github.com/whilp/cosmopolitan/releases/download/$(cosmos_version)
 
-$(eval $(call download_binary_rule,cosmos,lua,$(cosmos_url)/lua,089ac9689fdd5d522ecfd9940497528f0e5fa6f39945f5b1ffc8a60541bdb971))
-$(eval $(call download_binary_rule,cosmos,zip,$(cosmos_url)/zip,12bcc7b92c1d538f784bba74e00719230922074b49e777d608e6d8727d2ef0e1))
-$(eval $(call download_binary_rule,cosmos,unzip,$(cosmos_url)/unzip,c52a0bcaafda0484353677830895ef74e019c1b377a7831bf1422936769f34dd))
-$(eval $(call download_binary_rule,cosmos,make,$(cosmos_url)/make,b60bb21aee7c91a9f64db750859896900aa2a70262675ff629d0512f29f40268))
+$(eval $(call download_binary_rule,cosmos,lua,$(cosmos_url)/lua,0b571d8a5c4ebc5c23319e2fd06683656bf5e235bc70e9c24a9931b9e1768988))
+$(eval $(call download_binary_rule,cosmos,zip,$(cosmos_url)/zip,357ae4715d03e7eecfaf65645b8f5e7507f02258fb4296670e17776ffc39c380))
+$(eval $(call download_binary_rule,cosmos,unzip,$(cosmos_url)/unzip,54b5aa8b3ef97c4360430dd1325325c17745f3cbbc34740c2c6f02a779991f31))
+$(eval $(call download_binary_rule,cosmos,make,$(cosmos_url)/make,3804b0ed4dc50b10acdac766280d71fe066b349f456e10eb59cca4fbb456b569))
 
 cosmos_bin := $(cosmos_make_bin)
 
