60c187244074a5722d929f1d04a3dfebac979a5a
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-31 23:21:05 +0000

    ast-grep: fix test to use correct binary name (sg)

--- 3p/ast-grep/test.lua
+++ 3p/ast-grep/test.lua
@@ -2,7 +2,7 @@
 local spawn = require("spawn").spawn
 
 local function main(bin_dir)
-  local bin = bin_dir .. "/bin/ast-grep"
+  local bin = bin_dir .. "/bin/sg"
   local handle = spawn({bin, "--version"})
   return handle:wait() == 0
 end
