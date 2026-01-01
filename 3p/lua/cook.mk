aee7b87832b29d58fab37e807fd7ef22578ea718
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-01 00:30:17 +0000

    lua-dist: combine staging and ape steps

--- 3p/lua/cook.mk
+++ 3p/lua/cook.mk
@@ -1,14 +1,10 @@
 bins += o/%/lua-dist/bin/lua
 
-o/%/lua-dist/staging/.built: $(lib_libs) $(libs)
+o/%/lua-dist/bin/lua.ape: o/%/cosmos/bin/lua $(lib_libs) $(libs)
 	rm -rf o/$*/lua-dist/staging
-	mkdir -p o/$*/lua-dist/staging/.lua
+	mkdir -p o/$*/lua-dist/staging/.lua $(@D)
 	$(foreach d,$(3p_lib_dirs),cp -r $(subst %,$*,$(d))/* o/$*/lua-dist/staging/.lua/;)
 	$(foreach d,$(lib_dirs),cp -r $(d)/* o/$*/lua-dist/staging/.lua/;)
-	touch $@
-
-o/%/lua-dist/bin/lua.ape: o/%/cosmos/bin/lua o/%/lua-dist/staging/.built
-	mkdir -p $(@D)
 	cp o/$*/cosmos/bin/lua $@
 	chmod +x $@
 	cd o/$*/lua-dist/staging && zip -qr $(CURDIR)/$@ .lua
