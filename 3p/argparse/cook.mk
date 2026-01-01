a3ba97ac5e6c0e461cbc3118245950b229d02023
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-01 00:28:01 +0000

    argparse: add test

--- 3p/argparse/cook.mk
+++ 3p/argparse/cook.mk
@@ -2,6 +2,7 @@ argparse_version := 3p/argparse/version.lua
 lua_libs += argparse
 3p_lib_dirs += o/%/argparse/lib
 libs += o/%/argparse/lib/argparse.lua
+tests += o/%/argparse/test.ok
 
 o/%/argparse/archive.tar.gz: $(argparse_version) $(fetch)
 	$(fetch) $(argparse_version) $* $@
@@ -11,3 +12,6 @@ o/%/argparse/staging/src/argparse.lua: $(argparse_version) $(extract) o/%/argpar
 
 o/%/argparse/lib/argparse.lua: $(argparse_version) $(install) o/%/argparse/staging/src/argparse.lua
 	$(install) $(argparse_version) $* o/$*/argparse lib o/$*/argparse/staging/src/argparse.lua
+
+o/%/argparse/test.ok: 3p/argparse/test.lua o/%/argparse/lib/argparse.lua $(runner)
+	$(runner) $< $@
