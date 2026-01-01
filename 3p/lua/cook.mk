cc91436a56a4cdef0074245dfebd5fe0f84c0c0a
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-01 00:31:25 +0000

    lua-dist: output as o/%/lua.dist

--- 3p/lua/cook.mk
+++ 3p/lua/cook.mk
@@ -1,14 +1,14 @@
-bins += o/%/lua-dist/bin/lua
+bins += o/%/lua.dist
 
-o/%/lua-dist/bin/lua.ape: o/%/cosmos/bin/lua $(lib_libs) $(libs)
-	rm -rf o/$*/lua-dist/staging
-	mkdir -p o/$*/lua-dist/staging/.lua $(@D)
-	$(foreach d,$(3p_lib_dirs),cp -r $(subst %,$*,$(d))/* o/$*/lua-dist/staging/.lua/;)
-	$(foreach d,$(lib_dirs),cp -r $(d)/* o/$*/lua-dist/staging/.lua/;)
+o/%/lua.ape: o/%/cosmos/bin/lua $(lib_libs) $(libs)
+	rm -rf o/$*/lua-dist-staging
+	mkdir -p o/$*/lua-dist-staging/.lua
+	$(foreach d,$(3p_lib_dirs),cp -r $(subst %,$*,$(d))/* o/$*/lua-dist-staging/.lua/;)
+	$(foreach d,$(lib_dirs),cp -r $(d)/* o/$*/lua-dist-staging/.lua/;)
 	cp o/$*/cosmos/bin/lua $@
 	chmod +x $@
-	cd o/$*/lua-dist/staging && zip -qr $(CURDIR)/$@ .lua
+	cd o/$*/lua-dist-staging && zip -qr $(CURDIR)/$@ .lua
 
-o/%/lua-dist/bin/lua: o/%/lua-dist/bin/lua.ape
+o/%/lua.dist: o/%/lua.ape
 	cp $< $@
 	./$@ --assimilate || true
