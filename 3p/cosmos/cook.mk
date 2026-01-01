3a39e252936ecaf665a193198d821abe53fb7b19
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-01 00:04:12 +0000

    cosmos: add zip, unzip, make to bins

--- 3p/cosmos/cook.mk
+++ 3p/cosmos/cook.mk
@@ -1,5 +1,6 @@
 cosmos_version := 3p/cosmos/version.lua
-bins += o/%/cosmos/bin/lua
+cosmos_bins := o/%/cosmos/bin/lua o/%/cosmos/bin/zip o/%/cosmos/bin/unzip o/%/cosmos/bin/make
+bins += $(cosmos_bins)
 tests += o/%/cosmos/test.ok
 
 o/%/cosmos/archive.zip: $(cosmos_version) $(fetch)
@@ -8,10 +9,10 @@ o/%/cosmos/archive.zip: $(cosmos_version) $(fetch)
 o/%/cosmos/staging/lua: $(cosmos_version) $(extract) o/%/cosmos/archive.zip
 	$(extract) $(cosmos_version) $* o/$*/cosmos/archive.zip o/$*/cosmos/staging
 
-o/%/cosmos/bin/lua: $(cosmos_version) $(install) o/%/cosmos/staging/lua
-	$(install) $(cosmos_version) $* o/$*/cosmos bin o/$*/cosmos/staging/lua
+$(cosmos_bins): $(cosmos_version) $(install) o/%/cosmos/staging/lua
+	$(install) $(cosmos_version) $* o/$*/cosmos bin o/$*/cosmos/staging/$(@F)
 
-o/%/cosmos/test.ok: 3p/cosmos/test.lua o/%/cosmos/bin/lua $(runner)
+o/%/cosmos/test.ok: 3p/cosmos/test.lua $(cosmos_bins) $(runner)
 	TEST_BIN_DIR=o/$*/cosmos $(runner) $< $@
 
 cosmos-latest: | $(lua_bin)
--- 3p/cosmos/test.lua
+++ 3p/cosmos/test.lua
@@ -2,11 +2,26 @@ local lu = require("luaunit")
 local spawn = require("spawn")
 local path = require("cosmo.path")
 
-local bin = path.join(os.getenv("TEST_BIN_DIR"), "bin", "lua")
+local bin_dir = path.join(os.getenv("TEST_BIN_DIR"), "bin")
 
 TestCosmos = {}
 
-function TestCosmos:test_version()
-  local handle = spawn({ bin, "-v" })
+function TestCosmos:test_lua()
+  local handle = spawn({ path.join(bin_dir, "lua"), "-v" })
+  lu.assertEquals(handle:wait(), 0)
+end
+
+function TestCosmos:test_zip()
+  local handle = spawn({ path.join(bin_dir, "zip"), "--version" })
+  lu.assertEquals(handle:wait(), 0)
+end
+
+function TestCosmos:test_unzip()
+  local handle = spawn({ path.join(bin_dir, "unzip"), "-v" })
+  lu.assertEquals(handle:wait(), 0)
+end
+
+function TestCosmos:test_make()
+  local handle = spawn({ path.join(bin_dir, "make"), "-v" })
   lu.assertEquals(handle:wait(), 0)
 end
