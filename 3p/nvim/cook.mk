7a5e56d109dea75e3cc9d128a52a2c926e47481a
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-01 00:44:57 +0000

    nvim: add plugin fetching and bundling

--- 3p/nvim/cook.mk
+++ 3p/nvim/cook.mk
@@ -1,4 +1,8 @@
 nvim_version := 3p/nvim/version.lua
+nvim_pack_lock := .config/nvim/nvim-pack-lock.json
+nvim_plugins := $(shell lib/build/list-plugins.lua)
+nvim_fetch_plugin := lib/build/fetch-plugin.lua
+nvim_bundle := lib/build/nvim-bundle.lua
 bins += o/%/nvim/bin/nvim
 tests += o/%/nvim/test.ok
 
@@ -8,8 +12,16 @@ o/%/nvim/archive.tar.gz: $(nvim_version) $(fetch)
 o/%/nvim/staging/bin/nvim: $(nvim_version) $(extract) o/%/nvim/archive.tar.gz
 	$(extract) $(nvim_version) $* o/$*/nvim/archive.tar.gz o/$*/nvim/staging
 
-o/%/nvim/bin/nvim: $(nvim_version) $(install) o/%/nvim/staging/bin/nvim
+# Fetch each plugin (keep cached across builds)
+o/any/nvim/plugins/%: $(nvim_pack_lock) $(nvim_fetch_plugin)
+	$(nvim_fetch_plugin) $* $@
+
+nvim_plugin_dirs := $(addprefix o/any/nvim/plugins/,$(nvim_plugins))
+.PRECIOUS: $(nvim_plugin_dirs)
+
+o/%/nvim/bin/nvim: $(nvim_version) $(install) o/%/nvim/staging/bin/nvim $(nvim_plugin_dirs) $(nvim_bundle)
 	$(install) $(nvim_version) $* o/$*/nvim bin o/$*/nvim/staging/bin/nvim
+	$(nvim_bundle) $* o/$*/nvim o/any/nvim/plugins
 
 o/%/nvim/test.ok: 3p/nvim/test.lua o/%/nvim/bin/nvim $(runner)
 	TEST_BIN_DIR=o/$*/nvim $(runner) $< $@
