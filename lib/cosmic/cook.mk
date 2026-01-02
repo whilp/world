7643f00fb5e192d564481200ab23aacd8d828f9c
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-02 22:09:21 +0000

    cosmic: address review feedback on cook.mk
    - Remove explicit cosmos/bin/lua dep from test_spawn (uses TEST_ENV)
    - Define cosmic := $(o_platform)/cosmic/bin/cosmic variable
    - Build only for current platform (remove bins pattern)

--- lib/cosmic/cook.mk
+++ lib/cosmic/cook.mk
@@ -1,6 +1,7 @@
 # lib/cosmic - cosmopolitan lua utilities namespace
 
 cosmic_lib := $(o_any)/cosmic/lib
+cosmic := $(o_platform)/cosmic/bin/cosmic
 
 lib_lua_modules += cosmic
 lib_dirs += $(cosmic_lib)
@@ -16,31 +17,29 @@ $(cosmic_lib)/cosmic/%.lua: lib/cosmic/%.lua
 # test dependencies
 $(luatest_o)/lib/cosmic/test_cosmic.lua.ok: $(lib_libs)
 
-$(luatest_o)/lib/cosmic/test_spawn.lua.ok: $(cosmic_lib)/cosmic/spawn.lua o/$(current_platform)/cosmos/bin/lua
+$(luatest_o)/lib/cosmic/test_spawn.lua.ok: $(cosmic_lib)/cosmic/spawn.lua
 $(luatest_o)/lib/cosmic/test_spawn.lua.ok: TEST_ENV = TEST_BIN_DIR=o/$(current_platform)/cosmos
 
 $(luatest_o)/lib/cosmic/test_walk.lua.ok: $(cosmic_lib)/cosmic/walk.lua
 
-$(luatest_o)/lib/cosmic/test_binary.lua.ok: o/$(current_platform)/cosmic/bin/cosmic
-$(luatest_o)/lib/cosmic/test_binary.lua.ok: TEST_ENV = TEST_BIN_DIR=o/$(current_platform)/cosmic
+$(luatest_o)/lib/cosmic/test_binary.lua.ok: $(cosmic)
+$(luatest_o)/lib/cosmic/test_binary.lua.ok: TEST_ENV = TEST_BIN_DIR=$(o_platform)/cosmic
 
 # cosmic binary build
-bins += o/%/cosmic/bin/cosmic
-
 cosmic_lib_dirs := $(o_any)/lib $(cosmic_lib)
 cosmic_libs := $(lib_libs)
 
-o/%/cosmic/bin/cosmic: o/%/cosmos/bin/lua o/%/cosmos/bin/zip $(cosmic_libs) $(luaunit) o/%/argparse/lib/argparse.lua o/%/lfs/lib/lfs.lua
-	rm -rf o/$*/cosmic/staging
-	mkdir -p o/$*/cosmic/staging/.lua $(@D)
-	$(foreach d,$(cosmic_lib_dirs),cp -r $(d)/* o/$*/cosmic/staging/.lua/;)
-	cp -r $(o_any)/luaunit/lib/* o/$*/cosmic/staging/.lua/
-	cp -r o/$*/argparse/lib/* o/$*/cosmic/staging/.lua/
-	cp -r o/$*/lfs/lib/* o/$*/cosmic/staging/.lua/
-	cp o/$*/cosmos/bin/lua $@
+$(cosmic): $(o_platform)/cosmos/bin/lua $(o_platform)/cosmos/bin/zip $(cosmic_libs) $(luaunit) $(o_platform)/argparse/lib/argparse.lua $(o_platform)/lfs/lib/lfs.lua
+	rm -rf $(o_platform)/cosmic/staging
+	mkdir -p $(o_platform)/cosmic/staging/.lua $(@D)
+	$(foreach d,$(cosmic_lib_dirs),cp -r $(d)/* $(o_platform)/cosmic/staging/.lua/;)
+	cp -r $(o_any)/luaunit/lib/* $(o_platform)/cosmic/staging/.lua/
+	cp -r $(o_platform)/argparse/lib/* $(o_platform)/cosmic/staging/.lua/
+	cp -r $(o_platform)/lfs/lib/* $(o_platform)/cosmic/staging/.lua/
+	cp $(o_platform)/cosmos/bin/lua $@
 	chmod +x $@
-	cd o/$*/cosmic/staging && $(CURDIR)/o/$*/cosmos/bin/zip -qr $(CURDIR)/$@ .lua
+	cd $(o_platform)/cosmic/staging && $(CURDIR)/$(o_platform)/cosmos/bin/zip -qr $(CURDIR)/$@ .lua
 
-cosmic: o/$(current_platform)/cosmic/bin/cosmic ## Build cosmic for current platform
+cosmic: $(cosmic) ## Build cosmic for current platform
 
 .PHONY: cosmic
