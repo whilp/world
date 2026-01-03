bafca7ba7f75b9b5187dc9b086aedb8ff0111828
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 17:40:42 +0000

    cosmic: move lfs stub into lib/cosmic

--- lib/cosmic/cook.mk
+++ lib/cosmic/cook.mk
@@ -1,10 +1,11 @@
 modules += cosmic
 cosmic_srcs := $(wildcard lib/cosmic/*.lua)
 cosmic_tests := $(filter lib/cosmic/test_%.lua,$(cosmic_srcs))
-cosmic_libs := $(addprefix $(o)/,$(filter-out $(cosmic_tests),$(cosmic_srcs)))
+cosmic_libs := $(addprefix $(o)/,$(filter-out $(cosmic_tests) lib/cosmic/lfs.lua,$(cosmic_srcs)))
+cosmic_lfs := $(o)/lib/cosmic/lfs.lua
 cosmic_bin := $(o)/bin/cosmic
 cosmic_real := $(o)/bin/cosmic-real
-cosmic_files := $(cosmic_bin) $(cosmic_real) $(cosmic_libs)
+cosmic_files := $(cosmic_bin) $(cosmic_real) $(cosmic_libs) $(cosmic_lfs)
 cosmic_deps := cosmos luaunit argparse
 
 cosmic_built := $(o)/cosmic/.built
@@ -14,13 +15,13 @@ $(cosmic_bin): $(bootstrap_cosmic)
 	@mkdir -p $(@D)
 	@$(cp) $< $@
 
-$(cosmic_real): $(cosmic_libs)
+$(cosmic_real): $(cosmic_libs) $(cosmic_lfs)
 	@rm -rf $(cosmic_built)
 	@mkdir -p $(cosmic_built)/.lua/cosmic $(@D)
 	@$(cp) $(cosmic_libs) $(cosmic_built)/.lua/cosmic/
+	@$(cp) $(cosmic_lfs) $(cosmic_built)/.lua/
 	@$(cp) $(luaunit_dir)/*.lua $(cosmic_built)/.lua/
 	@$(cp) $(argparse_dir)/*.lua $(cosmic_built)/.lua/
-	@$(cp) 3p/lua/lfs_stub.lua $(cosmic_built)/.lua/lfs.lua
 	@$(cp) $(cosmos_dir)/lua $@
 	@chmod +x $@
 	@cd $(cosmic_built) && $(CURDIR)/$(cosmos_dir)/zip -qr $(CURDIR)/$@ .lua
