73265968dae1d8e5cca84d75323e734ff40d86e3
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 17:50:56 +0000

    cosmos: add cosmos_lua and cosmos_zip variables

--- 3p/cosmos/cook.mk
+++ 3p/cosmos/cook.mk
@@ -1,3 +1,5 @@
 modules += cosmos
 cosmos_version := 3p/cosmos/version.lua
 cosmos_tests := 3p/cosmos/test_cosmos.lua
+cosmos_lua = $(cosmos_dir)/lua
+cosmos_zip = $(cosmos_dir)/zip
--- lib/cosmic/cook.mk
+++ lib/cosmic/cook.mk
@@ -2,9 +2,10 @@ modules += cosmic
 cosmic_srcs := $(wildcard lib/cosmic/*.lua)
 cosmic_tests := $(filter lib/cosmic/test_%.lua,$(cosmic_srcs))
 cosmic_libs := $(addprefix $(o)/,$(filter-out $(cosmic_tests) lib/cosmic/lfs.lua,$(cosmic_srcs)))
+cosmic_lfs := $(o)/lib/cosmic/lfs.lua
 cosmic_bin := $(o)/bin/cosmic
 cosmic_real := $(o)/bin/cosmic-real
-cosmic_files := $(cosmic_bin) $(cosmic_real) $(addprefix $(o)/,$(filter-out $(cosmic_tests),$(cosmic_srcs)))
+cosmic_files := $(cosmic_bin) $(cosmic_real) $(cosmic_libs) $(cosmic_lfs)
 cosmic_deps := cosmos luaunit argparse
 
 cosmic_built := $(o)/cosmic/.built
@@ -12,13 +13,13 @@ cosmic_built := $(o)/cosmic/.built
 $(cosmic_bin): $(cosmic_real)
 	@$(cp) $< $@
 
-$(cosmic_real): $(cosmic_libs) $(o)/lib/cosmic/lfs.lua
+$(cosmic_real): $(cosmic_libs) $(cosmic_lfs)
 	@rm -rf $(cosmic_built)
 	@mkdir -p $(cosmic_built)/.lua/cosmic $(@D)
 	@$(cp) $(cosmic_libs) $(cosmic_built)/.lua/cosmic/
-	@$(cp) $(o)/lib/cosmic/lfs.lua $(cosmic_built)/.lua/
+	@$(cp) $(cosmic_lfs) $(cosmic_built)/.lua/
 	@$(cp) $(luaunit_dir)/*.lua $(cosmic_built)/.lua/
 	@$(cp) $(argparse_dir)/*.lua $(cosmic_built)/.lua/
-	@$(cp) $(cosmos_dir)/lua $@
+	@$(cp) $(cosmos_lua) $@
 	@chmod +x $@
-	@cd $(cosmic_built) && $(CURDIR)/$(cosmos_dir)/zip -qr $(CURDIR)/$@ .lua
+	@cd $(cosmic_built) && $(CURDIR)/$(cosmos_zip) -qr $(CURDIR)/$@ .lua
