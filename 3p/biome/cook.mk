d45f7885bb9b7b6474f0a342945df72fe51f2feb
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 18:36:39 +0000

    biome: port to new module pattern

--- 3p/biome/cook.mk
+++ 3p/biome/cook.mk
@@ -1,9 +1,3 @@
+modules += biome
 biome_version := 3p/biome/version.lua
-bins += o/%/biome/bin/biome
-
-
-o/%/biome/download: $(biome_version) $(fetch)
-	$(fetch) $(biome_version) $* $@
-
-o/%/biome/bin/biome: $(biome_version) $(install) o/%/biome/download
-	$(install) $(biome_version) $* o/$*/biome bin o/$*/biome/download
+biome_tests := 3p/biome/test_biome.lua
