8c1ce7b9c9bdd2dfc60b4f0284099435b17a06d2
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 18:08:25 +0000

    duckdb: port to new module pattern

--- 3p/duckdb/cook.mk
+++ 3p/duckdb/cook.mk
@@ -1,11 +1,3 @@
+modules += duckdb
 duckdb_version := 3p/duckdb/version.lua
-bins += o/%/duckdb/bin/duckdb
-
-o/%/duckdb/archive.zip: $(duckdb_version) $(fetch)
-	$(fetch) $(duckdb_version) $* $@
-
-o/%/duckdb/staging/duckdb: $(duckdb_version) $(extract) o/%/duckdb/archive.zip
-	$(extract) $(duckdb_version) $* o/$*/duckdb/archive.zip o/$*/duckdb/staging
-
-o/%/duckdb/bin/duckdb: $(duckdb_version) $(install) o/%/duckdb/staging/duckdb
-	$(install) $(duckdb_version) $* o/$*/duckdb bin o/$*/duckdb/staging/duckdb
+duckdb_tests := 3p/duckdb/test_duckdb.lua
---
+++ 3p/ruff/test_ruff.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/env run-test.lua
+
+local spawn = require("cosmic.spawn")
+local path = require("cosmo.path")
+
+local bin = path.join(TEST_DIR, "ruff")
+
+local function test_version()
+  local ok, got = spawn({ bin, "--version" }):read()
+  assert(ok, "ruff --version failed")
+  local want = "ruff"
+  assert(got:find(want, 1, true), "checking ruff, want: " .. want .. " got: " .. got)
+end
+test_version()
