d6e097c5c1a7b14d76727aa6bb9be7684b9296d0
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 18:37:10 +0000

    uv: port to new module pattern

--- 3p/uv/cook.mk
+++ 3p/uv/cook.mk
@@ -1,12 +1,3 @@
+modules += uv
 uv_version := 3p/uv/version.lua
-bins += o/%/uv/bin/uv
-
-
-o/%/uv/archive.tar.gz: $(uv_version) $(fetch)
-	$(fetch) $(uv_version) $* $@
-
-o/%/uv/staging/uv: $(uv_version) $(extract) o/%/uv/archive.tar.gz
-	$(extract) $(uv_version) $* o/$*/uv/archive.tar.gz o/$*/uv/staging
-
-o/%/uv/bin/uv: $(uv_version) $(install) o/%/uv/staging/uv
-	$(install) $(uv_version) $* o/$*/uv bin o/$*/uv/staging/uv
+uv_tests := 3p/uv/test_uv.lua
