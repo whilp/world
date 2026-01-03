2233f3040ab7c32eb03d6f8341fefb28fcfe588c
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 18:37:01 +0000

    stylua: port to new module pattern

--- 3p/stylua/cook.mk
+++ 3p/stylua/cook.mk
@@ -1,11 +1,3 @@
+modules += stylua
 stylua_version := 3p/stylua/version.lua
-bins += o/%/stylua/bin/stylua
-
-o/%/stylua/archive.zip: $(stylua_version) $(fetch)
-	$(fetch) $(stylua_version) $* $@
-
-o/%/stylua/staging/stylua: $(stylua_version) $(extract) o/%/stylua/archive.zip
-	$(extract) $(stylua_version) $* o/$*/stylua/archive.zip o/$*/stylua/staging
-
-o/%/stylua/bin/stylua: $(stylua_version) $(install) o/%/stylua/staging/stylua
-	$(install) $(stylua_version) $* o/$*/stylua bin o/$*/stylua/staging/stylua
+stylua_tests := 3p/stylua/test_stylua.lua
