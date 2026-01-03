53c452d600a8ba98bf7c944603ee9f5c876a77cc
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 18:37:00 +0000

    shfmt: port to new module pattern

--- 3p/shfmt/cook.mk
+++ 3p/shfmt/cook.mk
@@ -1,9 +1,3 @@
+modules += shfmt
 shfmt_version := 3p/shfmt/version.lua
-bins += o/%/shfmt/bin/shfmt
-
-
-o/%/shfmt/download: $(shfmt_version) $(fetch)
-	$(fetch) $(shfmt_version) $* $@
-
-o/%/shfmt/bin/shfmt: $(shfmt_version) $(install) o/%/shfmt/download
-	$(install) $(shfmt_version) $* o/$*/shfmt bin o/$*/shfmt/download
+shfmt_tests := 3p/shfmt/test_shfmt.lua
