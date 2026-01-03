20787f50c694e0306715bbf9c4e2e4b0e29d071f
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 17:39:16 +0000

    cosmic: remove dead code and reorder variables

--- lib/cosmic/cook.mk
+++ lib/cosmic/cook.mk
@@ -1,18 +1,18 @@
 modules += cosmic
-cosmic_bin := $(o)/bin/cosmic
-cosmic_real := $(o)/bin/cosmic-real
 cosmic_srcs := $(wildcard lib/cosmic/*.lua)
 cosmic_tests := $(filter lib/cosmic/test_%.lua,$(cosmic_srcs))
 cosmic_libs := $(addprefix $(o)/,$(filter-out $(cosmic_tests),$(cosmic_srcs)))
+cosmic_bin := $(o)/bin/cosmic
+cosmic_real := $(o)/bin/cosmic-real
 cosmic_files := $(cosmic_bin) $(cosmic_real) $(cosmic_libs)
 cosmic_deps := cosmos luaunit argparse
 
-# TODO: build cosmic properly
+cosmic_built := $(o)/cosmic/.built
+
+# TODO: build cosmic properly from cosmic_libs
 $(cosmic_bin): $(bootstrap_cosmic)
 	@mkdir -p $(@D)
 	@$(cp) $< $@
-
-cosmic_built := $(o)/cosmic/.built
 
 $(cosmic_real): $(cosmic_libs)
 	@rm -rf $(cosmic_built)
@@ -24,19 +24,3 @@ $(cosmic_real): $(cosmic_libs)
 	@$(cp) $(cosmos_dir)/lua $@
 	@chmod +x $@
 	@cd $(cosmic_built) && $(CURDIR)/$(cosmos_dir)/zip -qr $(CURDIR)/$@ .lua
-
-# cosmic binary
-
-#$(cosmic_bin): $(o)/$(platform)/cosmos/bin/lua $(o)/$(platform)/cosmos/bin/zip $(cosmic_files) $(luaunit_staged) $(o)/$(platform)/argparse/lib/argparse.lua $(o)/$(platform)/lfs/lib/lfs.lua
-#	@rm -rf $(o)/any/cosmic/staging
-#	@mkdir -p $(o)/any/cosmic/staging/.lua $(@D)
-#	@cp -r $(o)/lib/cosmic/* $(o)/any/cosmic/staging/.lua/
-#	@cp -r $(luaunit_staged)/lib/* $(o)/any/cosmic/staging/.lua/
-#	@cp -r $(o)/$(platform)/argparse/lib/* $(o)/any/cosmic/staging/.lua/
-#	@cp -r $(o)/$(platform)/lfs/lib/* $(o)/any/cosmic/staging/.lua/
-#	@cp $(o)/$(platform)/cosmos/bin/lua $@
-#	@chmod +x $@
-#	@cd $(o)/any/cosmic/staging && $(CURDIR)/$(o)/$(platform)/cosmos/bin/zip -qr $(CURDIR)/$@ .lua
-#
-#.PHONY: cosmic
-#cosmic: $(cosmic_bin) ## Build cosmic binary
