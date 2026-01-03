c974952c25bd7bf74a9a53e5491c7d0fa18c01e2
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-03 14:54:54 +0000

    cosmic: add binary build recipe

--- lib/cosmic/cook.mk
+++ lib/cosmic/cook.mk
@@ -1,3 +1,20 @@
 modules += cosmic
 cosmic_files := $(addprefix $(o)/lib/cosmic/,init.lua spawn.lua walk.lua help.lua)
 cosmic_tests := $(wildcard lib/cosmic/test_*.lua)
+
+# cosmic binary
+cosmic_bin := $(o)/any/cosmic/bin/cosmic
+
+$(cosmic_bin): $(o)/$(platform)/cosmos/bin/lua $(o)/$(platform)/cosmos/bin/zip $(cosmic_files) $(luaunit_staged) $(o)/$(platform)/argparse/lib/argparse.lua $(o)/$(platform)/lfs/lib/lfs.lua
+	@rm -rf $(o)/any/cosmic/staging
+	@mkdir -p $(o)/any/cosmic/staging/.lua $(@D)
+	@cp -r $(o)/lib/cosmic/* $(o)/any/cosmic/staging/.lua/
+	@cp -r $(luaunit_staged)/lib/* $(o)/any/cosmic/staging/.lua/
+	@cp -r $(o)/$(platform)/argparse/lib/* $(o)/any/cosmic/staging/.lua/
+	@cp -r $(o)/$(platform)/lfs/lib/* $(o)/any/cosmic/staging/.lua/
+	@cp $(o)/$(platform)/cosmos/bin/lua $@
+	@chmod +x $@
+	@cd $(o)/any/cosmic/staging && $(CURDIR)/$(o)/$(platform)/cosmos/bin/zip -qr $(CURDIR)/$@ .lua
+
+.PHONY: cosmic
+cosmic: $(cosmic_bin) ## Build cosmic binary
