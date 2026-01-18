f218868a92261bd440ee02927da55d5fb09b76c8
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-17 19:10:31 -0800

    cosmos: bump to 2026.01.18 (#393)

--- 3p/cosmos/version.lua
+++ 3p/cosmos/version.lua
@@ -1,7 +1,7 @@
 return {
   format="zip",
-  platforms={["*"]={sha="661d9b89b1742f258edccd76dadc78ee6fbd5f8cfc980f28a800542229639357"}},
+  platforms={["*"]={sha="5f10a044d5f12406e3c83b3ec3d8345309e59feffb1b1443d406aad3591d5363"}},
   strip_components=0,
   url="https://github.com/whilp/cosmopolitan/releases/download/{version}/cosmos.zip",
-  version="2026.01.14-dff272e74"
+  version="2026.01.18-9df0a3c58"
 }
