8f93164cd129b0ef2e26268954aea93158a27289
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-19 14:28:35 -0800

    cosmic: update to 2026-01-19-f861075 (#440)
    * cosmic: update to 2026-01-19-f861075
    
    * bootstrap: curl cosmic directly in makefile

--- 3p/cosmic/version.lua
+++ 3p/cosmic/version.lua
@@ -1,8 +1,8 @@
 return {
-  version = "2026-01-19-1c3a9af",
+  version = "2026-01-19-f861075",
   format = "binary",
   url = "https://github.com/whilp/cosmic/releases/download/{version}/cosmic-lua",
   platforms = {
-    ["*"] = { sha = "cacadf2b51b5ecda74713163930e42a8b2779b21246845906b19bb0678a72185" },
+    ["*"] = { sha = "4c881a10a68dc8638fde1deda4400324a5a87fb56c54631e46248fb03b9b8058" },
   },
 }
