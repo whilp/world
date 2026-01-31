1ccf91292df0a758c5d71075cd96d2645d4d29a0
Will Maier <189851+whilp@users.noreply.github.com>
2026-01-30 18:48:17 -0800

    cosmic: bump to 2026-01-30-8e737d0
    Committed-By-Agent: claude

--- 3p/cosmic/version.lua
+++ 3p/cosmic/version.lua
@@ -1,8 +1,8 @@
 return {
-  version = "2026-01-21-ce05821",
+  version = "2026-01-30-8e737d0",
   format = "binary",
   url = "https://github.com/whilp/cosmic/releases/download/{version}/cosmic-lua",
   platforms = {
-    ["*"] = { sha = "d76bc287d176e3f1a3bf7b198a4199979f9529ff071b718759b69a20e7726873" },
+    ["*"] = { sha = "895e03738fd5111748fdc3475ae1e72b4fefaeb1b592dbd63aa2e983167296bd" },
   },
 }
